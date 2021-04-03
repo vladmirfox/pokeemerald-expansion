d3065dd853dc153548ea6f0ecc89fb833659284b
ExpoSeed <43502820+ExpoSeed@users.noreply.github.com>
2021-04-03 00:55:59 -0500

    re-re-remove semicolons

--- src/AgbRfu_LinkManager.c
+++ src/AgbRfu_LinkManager.c
@@ -14,7 +14,7 @@
 #define FSP_ON    0x01
 #define FSP_START 0x02
 
-LINK_MANAGER lman;
+LINK_MANAGER lman
 
 static void rfu_LMAN_clearVariables(void);
 static void rfu_LMAN_settingPCSWITCH(u32 rand);
