76e4314a818522bd357c68ad05e5593addbfefb3
WiserVisor <63819738+WiserVisor@users.noreply.github.com>
2021-05-23 22:31:26 -0700

    Added VARIOUS command constants for Sky Drop.

--- include/constants/battle_script_commands.h
+++ include/constants/battle_script_commands.h
@@ -173,6 +173,9 @@
 #define VARIOUS_DESTROY_ABILITY_POPUP           102
 #define VARIOUS_TOTEM_BOOST                     103
 #define VARIOUS_TRY_ACTIVATE_GRIM_NEIGH         104
+#define VARIOUS_JUMP_IF_UNDER_200               105
+#define VARIOUS_SET_SKY_DROP                    106
+#define VARIOUS_CLEAR_SKY_DROP                  107
 
 // Cmd_manipulatedamage
 #define DMG_CHANGE_SIGN            0
