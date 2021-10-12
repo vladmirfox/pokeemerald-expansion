5e54985dfdb6e83169bddac3793d4fac8beebcd7
WiserVisor <63819738+WiserVisor@users.noreply.github.com>
2021-10-12 14:59:36 -0700

    Merge conflict fix

--- include/battle.h
+++ include/battle.h
@@ -602,6 +602,7 @@ struct BattleStruct
     u8 quickClawBattlerId;
     struct StolenItem itemStolen[PARTY_SIZE];  // Player's team that had items stolen (two bytes per party member)
     u8 blunderPolicy:1; // should blunder policy activate
+    u8 ballSpriteIds[2];    // item gfx, window gfx
     u8 skyDropTargets[MAX_BATTLERS_COUNT]; // For Sky Drop, to account for if multiple Pokemon use Sky Drop in a double battle.
 };
 
