53118dfffb0d11745cbc1a6a0a16fd4cd4c141fe
kleeenexfeu <94004034+kleeenexfeu@users.noreply.github.com>
2021-11-17 20:48:45 +0100

    variable decalaration

--- src/battle_main.c
+++ src/battle_main.c
@@ -4702,6 +4702,7 @@ static void CheckMegaEvolutionBeforeTurn(void)
 // In gen7, priority and speed is recalculated during the turn in which a pokemon mega evolve
 static void TryChangeTurnOrder(void)
 {
+    s32 i, j;
     for (i = 0; i < gBattlersCount - 1; i++)
     {
         for (j = i + 1; j < gBattlersCount; j++)
