e17c25cb0451ad3151465460612e90feeb55da50
AgustinGDLV <agustingomezdelavega@gmail.com>
2022-04-19 19:45:43 -0700

    Undoing test change to wild_encounter.c.

--- src/wild_encounter.c
+++ src/wild_encounter.c
@@ -272,11 +272,6 @@ static u8 ChooseWildMonLevel(const struct WildPokemon *wildPokemon)
     u8 range;
     u8 rand;
     
-    if (GetMonData(&gPlayerParty[0], MON_DATA_LEVEL) == 6)
-    {
-        return 10;
-    }
-
     // Make sure minimum level is less than maximum level
     if (wildPokemon->maxLevel >= wildPokemon->minLevel)
     {
