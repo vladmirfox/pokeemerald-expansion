387b2ed2ba23b19f2e367214e3020582f45b5337
HunarPG <92902941+HunarPG@users.noreply.github.com>
2022-05-26 19:51:36 +0530

    LGPE-style Premier Balls

--- src/strings.c
+++ src/strings.c
@@ -1820,3 +1820,4 @@ const u8 gText_PokeBalls[] = _("POKé BALLS");
 const u8 gText_Berry[] = _("BERRY");
 const u8 gText_Berries[] = _("BERRIES");
 const u8 gText_Nickname[] = _("NICKNAME");
+const u8 gText_ThrowInPremierBalls[] = _("I'll throw in some Premier Balls, too.{PAUSE_UNTIL_PRESS}");
