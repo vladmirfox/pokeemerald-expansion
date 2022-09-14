4a36b69954bff4161fffc83842137eb830e8b351
Alaxandir <jalex01x@gmail.com>
2022-09-13 23:34:07 -0500

    tidying, post make

--- include/constants/map_groups.h
+++ include/constants/map_groups.h
@@ -625,4 +625,6 @@
 
 #define MAP_GROUPS_COUNT 37
 
+// static const u8 MAP_GROUP_COUNT[] = {57, 8, 10, 7, 6, 5, 6, 7, 8, 9, 7, 7, 14, 8, 17, 10, 23, 13, 15, 15, 2, 2, 2, 3, 1, 1, 1, 108, 61, 89, 2, 1, 13, 1, 1, 3, 1, 0};
+
 #endif // GUARD_CONSTANTS_MAP_GROUPS_H
