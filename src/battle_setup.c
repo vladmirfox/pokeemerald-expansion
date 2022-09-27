7045ccd5a33dff44be98905a8ea593f577615cec
TheXaman <48356183+TheXaman@users.noreply.github.com>
2022-09-27 22:10:22 +0200

    added battle start
    # Conflicts:
    #	include/debug.h
    #	src/battle_ai_script_commands.c

--- src/battle_setup.c
+++ src/battle_setup.c
@@ -1397,11 +1397,6 @@ void BattleSetup_StartTrainerBattle(void)
 
 void BattleSetup_StartTrainerBattle_Debug(void)
 {
-    u8 i;
-
-    for (i = 0; i < PARTY_SIZE; i++)
-        ZeroMonData(&gEnemyParty[i]);
-
     sNoOfPossibleTrainerRetScripts = gNoOfApproachingTrainers;
     gNoOfApproachingTrainers = 0;
     sShouldCheckTrainerBScript = FALSE;
