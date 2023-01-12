015d3323ba750150733e795651dbd0bea6232e4f
AaghatIsLive <adventureco36@gmail.com>
2023-01-12 23:50:36 +0530

    Added Pallet town scripts

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -41,13 +41,15 @@
 #define FLAG_TEMP_1F     (TEMP_FLAGS_START + 0x1F)
 #define TEMP_FLAGS_END   FLAG_TEMP_1F
 
-#define FLAG_UNUSED_0x020    0x20 // Unused Flag
-#define FLAG_UNUSED_0x021    0x21 // Unused Flag
-#define FLAG_UNUSED_0x022    0x22 // Unused Flag
+//FR flags for Proxj
+
+#define FLAG_HIDE_OAK_IN_PALLET_TOWN    0x20 // Unused Flag
+#define FLAG_PALLET_LADY_NOT_BLOCKING_SIGN    0x21 // Unused Flag
+#define FLAG_OPENED_START_MENU    0x22 // Unused Flag
 #define FLAG_UNUSED_0x023    0x23 // Unused Flag
 #define FLAG_UNUSED_0x024    0x24 // Unused Flag
-#define FLAG_UNUSED_0x025    0x25 // Unused Flag
-#define FLAG_UNUSED_0x026    0x26 // Unused Flag
+#define FLAG_WORLD_MAP_ONE_ISLAND    0x25 // Unused Flag
+#define FLAG_HIDE_OAK_IN_HIS_LAB    0x26 // Unused Flag
 #define FLAG_UNUSED_0x027    0x27 // Unused Flag
 #define FLAG_UNUSED_0x028    0x28 // Unused Flag
 #define FLAG_UNUSED_0x029    0x29 // Unused Flag
@@ -651,7 +653,10 @@
 #define FLAG_HIDDEN_ITEM_ROUTE_123_RARE_CANDY                (FLAG_HIDDEN_ITEMS_START + 0x6E)
 #define FLAG_HIDDEN_ITEM_ROUTE_105_BIG_PEARL                 (FLAG_HIDDEN_ITEMS_START + 0x6F)
 
-#define FLAG_UNUSED_0x264  0x264 // Unused Flag
+
+//FR system Proxj flags
+
+#define FLAG_WORLD_MAP_PALLET_TOWN  0x264 // Unused Flag
 #define FLAG_UNUSED_0x265  0x265 // Unused Flag
 #define FLAG_UNUSED_0x266  0x266 // Unused Flag
 #define FLAG_UNUSED_0x267  0x267 // Unused Flag
