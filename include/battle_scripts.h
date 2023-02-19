695e0b7ad2fced03bf9d8bd9885de2bcad7cf653
DizzyEggg <jajkodizzy@wp.pl>
2023-02-19 13:33:29 +0100

    fix merge conflicts

--- include/battle_scripts.h
+++ include/battle_scripts.h
@@ -426,7 +426,6 @@ extern const u8 BattleScript_MagicianActivates[];
 extern const u8 BattleScript_BeakBlastSetUp[];
 extern const u8 BattleScript_BeakBlastBurn[];
 extern const u8 BattleScript_DefDownSpeedUp[];
-<<<<<<< HEAD
 extern const u8 BattleScript_AffectionBasedStatusHeal[];
 extern const u8 BattleScript_AffectionBasedEndurance[];
 extern const u8 BattleScript_SymbiosisActivates[];
