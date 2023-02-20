65cb4f0aeb2a3f792bc84e2d45179f6f655d0023
Martin Griffin <martinrgriffin@gmail.com>
2023-02-20 00:47:56 +0000

    Cleanup Healing Wish test syntax

--- test/move_effect_healing_wish.c
+++ test/move_effect_healing_wish.c
@@ -13,11 +13,11 @@ SINGLE_BATTLE_TEST("Healing Wish causes the user to faint and fully heals the re
 {
     GIVEN {
         ASSUME(B_HEALING_WISH_SWITCH >= GEN_5);
-        PLAYER(SPECIES_GARDEVOIR); // Faster than Wobbufeet.
-        PLAYER(SPECIES_WYNAUT) {HP(1) ; MaxHP(TEST_MAX_HP); Status1(STATUS1_POISON); };
+        PLAYER(SPECIES_GARDEVOIR);
+        PLAYER(SPECIES_WYNAUT) { HP(1); MaxHP(TEST_MAX_HP); Status1(STATUS1_POISON); }
         OPPONENT(SPECIES_WOBBUFFET);
     } WHEN {
-        TURN { MOVE(player, MOVE_HEALING_WISH); SEND_OUT(player, 1) ; }
+        TURN { MOVE(player, MOVE_HEALING_WISH); SEND_OUT(player, 1); }
     } SCENE {
         ANIMATION(ANIM_TYPE_MOVE, MOVE_HEALING_WISH, player);
         HP_BAR(player, hp: 0);
@@ -33,13 +33,13 @@ DOUBLE_BATTLE_TEST("Lunar Dance causes the user to faint and fully heals the rep
 {
     GIVEN {
         ASSUME(B_HEALING_WISH_SWITCH >= GEN_5);
-        PLAYER(SPECIES_GARDEVOIR) {Speed(300); }; // Faster than Wobbufeet.
-        PLAYER(SPECIES_WOBBUFFET) {Speed(50); };
-        PLAYER(SPECIES_WYNAUT) {HP(TEST_MAX_HP - 1) ; MaxHP(TEST_MAX_HP); Status1(STATUS1_BURN); Speed(50); };
-        OPPONENT(SPECIES_WOBBUFFET) {Speed(50); };
-        OPPONENT(SPECIES_WOBBUFFET) {Speed(50); };
+        PLAYER(SPECIES_GARDEVOIR) { Speed(300); }
+        PLAYER(SPECIES_WOBBUFFET) { Speed(50); }
+        PLAYER(SPECIES_WYNAUT) { HP(TEST_MAX_HP - 1); MaxHP(TEST_MAX_HP); Status1(STATUS1_BURN); Speed(50); }
+        OPPONENT(SPECIES_WOBBUFFET) { Speed(50); }
+        OPPONENT(SPECIES_WOBBUFFET) { Speed(50); }
     } WHEN {
-        TURN { MOVE(playerLeft, MOVE_LUNAR_DANCE); SEND_OUT(playerLeft, 2) ; }
+        TURN { MOVE(playerLeft, MOVE_LUNAR_DANCE); SEND_OUT(playerLeft, 2); }
     } SCENE {
         ANIMATION(ANIM_TYPE_MOVE, MOVE_LUNAR_DANCE, playerLeft);
         HP_BAR(playerLeft, hp: 0);
@@ -55,18 +55,21 @@ SINGLE_BATTLE_TEST("Healing Wish effect activates only if the switched pokemon c
 {
     GIVEN {
         ASSUME(B_HEALING_WISH_SWITCH >= GEN_8);
-        PLAYER(SPECIES_GARDEVOIR) {Speed(300); }; // Faster than Wobbufeet.
-        PLAYER(SPECIES_NINJASK) {Speed(400); }; // Healthy Ninjask
-        PLAYER(SPECIES_WYNAUT) {HP(TEST_MAX_HP / 2) ; MaxHP(TEST_MAX_HP); Status1(STATUS1_PARALYSIS); {Speed(50); } };
-        OPPONENT(SPECIES_WOBBUFFET) {Speed(50); };
+        PLAYER(SPECIES_GARDEVOIR) { Speed(300); }
+        PLAYER(SPECIES_NINJASK) { Speed(400); }
+        PLAYER(SPECIES_WYNAUT) { HP(TEST_MAX_HP / 2); MaxHP(TEST_MAX_HP); Status1(STATUS1_PARALYSIS); Speed(50); }
+        OPPONENT(SPECIES_WOBBUFFET) {Speed(50); }
     } WHEN {
-        TURN { MOVE(player, MOVE_HEALING_WISH); SEND_OUT(player, 1) ; }
-        TURN { MOVE(player, MOVE_U_TURN); SEND_OUT(player, 2) ; }
+        TURN { MOVE(player, MOVE_HEALING_WISH); SEND_OUT(player, 1); }
+        TURN { MOVE(player, MOVE_U_TURN); SEND_OUT(player, 2); }
     } SCENE {
         ANIMATION(ANIM_TYPE_MOVE, MOVE_HEALING_WISH, player);
         HP_BAR(player, hp: 0);
         MESSAGE("Gardevoir fainted!");
-        NONE_OF {MESSAGE("The healing wish came true for Wynaut!") ; MESSAGE("Wynaut regained health!");};
+        NONE_OF {
+            MESSAGE("The healing wish came true for Wynaut!");
+            MESSAGE("Wynaut regained health!");
+        }
         ANIMATION(ANIM_TYPE_MOVE, MOVE_U_TURN, player);
         MESSAGE("Do it! Wynaut!");
         MESSAGE("The healing wish came true for Wynaut!");
