88aeb4586c2e1cbb20ed2c3bff94be2efbd08589
Philipp AUER <SBird1337@users.noreply.github.com>
2023-03-20 20:27:16 +0100

    Update test/hold_effect_eject_button.c

--- test/hold_effect_eject_button.c
+++ test/hold_effect_eject_button.c
@@ -113,9 +113,9 @@ SINGLE_BATTLE_TEST("Eject Button is not triggered after the mon loses Eject Butt
 SINGLE_BATTLE_TEST("Eject Button is not triggered after given to player by Picketpocket")
 {
     GIVEN {
-        PLAYER(SPECIES_REGIELEKI) { Item(ITEM_EJECT_BUTTON); }
+        PLAYER(SPECIES_REGIELEKI) { Item(ITEM_EJECT_BUTTON); };
         PLAYER(SPECIES_WOBBUFFET);
-        OPPONENT(SPECIES_SNEASEL) { Ability(ABILITY_PICKPOCKET); }
+        OPPONENT(SPECIES_SNEASEL) { Ability(ABILITY_PICKPOCKET); };
         OPPONENT(SPECIES_WOBBUFFET);
     } WHEN {
         TURN {
