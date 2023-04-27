94b40e5b5b9d1cbc0f426422edeba96dd0dc1264
ShaeTsuPog <shaetsu@protonmail.com>
2023-04-27 05:42:20 -0700

    Add generational config for Leaf Guard

--- data/battle_scripts_1.s
+++ data/battle_scripts_1.s
@@ -3969,7 +3969,9 @@ BattleScript_EffectRest::
 	jumpifuproarwakes BattleScript_RestCantSleep
 	jumpifability BS_TARGET, ABILITY_INSOMNIA, BattleScript_InsomniaProtects
 	jumpifability BS_TARGET, ABILITY_VITAL_SPIRIT, BattleScript_InsomniaProtects
+.if B_LEAF_GUARD_PREVENTS_REST >= GEN_5
 	jumpifleafguardprotected BS_TARGET, BattleScript_LeafGuardPreventsRest
+.endif
 	trysetrest BattleScript_AlreadyAtFullHp
 	pause B_WAIT_TIME_SHORT
 	printfromtable gRestUsedStringIds
