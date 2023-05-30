b6df5493f493c2202f444324a40fb3aba6f49948
Bassoonian <iasperbassoonian@gmail.com>
2023-05-30 22:01:00 +0200

    Improve comparisons

--- make_release.py
+++ make_release.py
@@ -1,8 +1,9 @@
-from pcpp import Preprocessor
+from pcpp import Preprocessor, Evaluator
 from cxxheaderparser.simple import parse_file
 import os
 
 globalVersion = 0
+globalHasChanges = False
 
 def preprocess_files(filename, version):
 # preprocess a file and store it as files in /versioning with pcpp
@@ -21,11 +22,24 @@ def pull_new_version():
         globalVersion += 1
     preprocess_files("global", globalVersion)
     preprocess_files("pokemon_storage_system", globalVersion)
+    if (globalVersion == 0):
+        print("v0 saved. please make changes in order to enable v1")
+        quit()
+
+def parse_field(field):
+    cl = {}
+    e = Evaluator()
+    cl['type'] = field.type.__class__.__name__
+    if hasattr(field.type, 'size'):
+        sizetokens = ""
+        for x in field.type.size.tokens:
+            sizetokens += str(x.value)
+        cl['size'] = e(sizetokens)
+    if (field.bits != None):
+        cl['bits'] = field.bits
+    return(cl)
 
 pull_new_version()
-if (globalVersion == 0):
-    print("v0 saved. please make changes in order to enable v1")
-    quit()
 
 # use cxxheaderparser to figure out everything
 classes_old = {}
@@ -62,27 +76,39 @@ def compareFields(fieldname):
     # compare
     print("Comparing %s" % fieldname)
     for x in classes_new[fieldname].fields:
+        oldclass = parse_field(fields_old[x.name])
+        newclass = parse_field(x)
+
         if (x.name in fields_old_array):
             fields_old_array.remove(x.name)
             if (x == fields_old[x.name]):
                 # print("%s is identical" % x.name)
                 continue
             # figure out what exactly is different, starting with size
-            oldsize = fields_old[x.name].type.size.tokens[0].value
-            newsize = x.type.size.tokens[0].value
-            if (oldsize != newsize):
-                if (oldsize < newsize):
-                    print("%s was expanded in size from %s to %s" % (x.name, oldsize, newsize))
-                else:
-                    print("IMPORTANT: %s was truncated in size from %s to %s" % (x.name, oldsize, newsize))
-                continue
-            print("%s is different!" % x.name)
+            global globalHasChanges
+            globalHasChanges = True
+            if 'size' in newclass and 'size' in oldclass:
+                oldsize = oldclass['size']
+                newsize = newclass['size']
+                if (oldsize != newsize):
+                    if (oldsize < newsize):
+                        print("%s was expanded in size from %s to %s" % (x.name, oldsize, newsize))
+                    else:
+                        print("IMPORTANT: %s was truncated in size from %s to %s" % (x.name, oldsize, newsize))
+                    continue
+            print("%s is different, but can't identify why!" % x.name)
         else:
             print("%s is a new field; defaulting values to zero" % x.name)
 
     if len(fields_old_array) > 0:
         print("The following old fields are not retained: %s" % fields_old_array)
 
-compareFields('SaveBlock1')
 compareFields('SaveBlock2')
+compareFields('SaveBlock1')
 compareFields('PokemonStorage')
+
+# clean up if no changes
+if not globalHasChanges:
+    print("No save migration needed!")
+    os.remove("versioning/global_v%s.c" % globalVersion)
+    os.remove("versioning/pokemon_storage_system_v%s.c" % globalVersion)
