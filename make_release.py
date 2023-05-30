767c71560ec5af596a1aad9959125045cd6c668f
Bassoonian <iasperbassoonian@gmail.com>
2023-05-30 21:34:03 +0200

    Some restructuring

--- make_release.py
+++ make_release.py
@@ -1,21 +1,49 @@
 from pcpp import Preprocessor
 from cxxheaderparser.simple import parse_file
+import os
 
-def grab_new_version():
-# preprocess global.h and store it as a file in /versioning with pcpp
+globalVersion = 0
+
+def preprocess_files(filename, version):
+# preprocess a file and store it as files in /versioning with pcpp
     p = Preprocessor()
-    path = "include/global.h"
-
+    path = "include/%s.h" % filename
     with open(path, 'rt') as ih:
         p.parse(ih.read(), path)
-    with open('versioning/0.c', 'w') as oh:
+    with open('versioning/%s_v%s.c' % (filename, version), 'w') as oh:
         p.write(oh)
 
+def pull_new_version():
+    if not os.path.exists("versioning/"):
+        os.mkdir("versioning/")
+    global globalVersion
+    while os.path.exists("versioning/global_v%s.c" % globalVersion):
+        globalVersion += 1
+    preprocess_files("global", globalVersion)
+    preprocess_files("pokemon_storage_system", globalVersion)
+
+pull_new_version()
+if (globalVersion == 0):
+    print("v0 saved. please make changes in order to enable v1")
+    quit()
+
 # use cxxheaderparser to figure out everything
-contents_old = parse_file('versioning/0.c')
-contents_new = parse_file('versioning/1.c')
 classes_old = {}
 classes_new = {}
+
+# global
+contents_old = parse_file('versioning/global_v0.c')
+contents_new = parse_file('versioning/global_v1.c')
+for x in contents_old.namespace.classes:
+    if hasattr(x.class_decl.typename.segments[0], 'name'):
+        classes_old[x.class_decl.typename.segments[0].name] = x
+for x in contents_new.namespace.classes:
+    if hasattr(x.class_decl.typename.segments[0], 'name'):
+        classes_new[x.class_decl.typename.segments[0].name] = x
+
+# pokemon_storage_system
+contents_old = parse_file('versioning/pokemon_storage_system_v0.c')
+contents_new = parse_file('versioning/pokemon_storage_system_v1.c')
 for x in contents_old.namespace.classes:
     if hasattr(x.class_decl.typename.segments[0], 'name'):
         classes_old[x.class_decl.typename.segments[0].name] = x
@@ -35,21 +63,20 @@ def compareFields(fieldname):
     print("Comparing %s" % fieldname)
     for x in classes_new[fieldname].fields:
         if (x.name in fields_old_array):
+            fields_old_array.remove(x.name)
             if (x == fields_old[x.name]):
                 # print("%s is identical" % x.name)
-                fields_old_array.remove(x.name)
-            else:
-                # figure out what exactly is different, starting with size
-                oldsize = fields_old[x.name].type.size.tokens[0].value
-                newsize = x.type.size.tokens[0].value
-                if (oldsize != newsize):
-                    if (oldsize < newsize):
-                        print("%s was expanded in size from %s to %s" % (x.name, oldsize, newsize))
-                    else:
-                        print("IMPORTANT: %s was truncated in size from %s to %s" % (x.name, oldsize, newsize))
-
-                print("%s is different!" % x.name)
-                fields_old_array.remove(x.name)
+                continue
+            # figure out what exactly is different, starting with size
+            oldsize = fields_old[x.name].type.size.tokens[0].value
+            newsize = x.type.size.tokens[0].value
+            if (oldsize != newsize):
+                if (oldsize < newsize):
+                    print("%s was expanded in size from %s to %s" % (x.name, oldsize, newsize))
+                else:
+                    print("IMPORTANT: %s was truncated in size from %s to %s" % (x.name, oldsize, newsize))
+                continue
+            print("%s is different!" % x.name)
         else:
             print("%s is a new field; defaulting values to zero" % x.name)
 
@@ -58,3 +85,4 @@ def compareFields(fieldname):
 
 compareFields('SaveBlock1')
 compareFields('SaveBlock2')
+compareFields('PokemonStorage')
