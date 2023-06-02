b0ddd6a1853026ec448674207d2b9694f59e6aef
Bassoonian <iasperbassoonian@gmail.com>
2023-06-02 11:14:41 +0200

    Enable walking

--- make_release.py
+++ make_release.py
@@ -2,6 +2,8 @@ from pcpp import Preprocessor, Evaluator
 from cxxheaderparser.simple import parse_file, CxxParser, SimpleCxxVisitor
 import os
 
+trusted_typedefs = ['u8', 'u16', 'u32', 'u64', 's8', 's16', 's32', 's64', 'bool8', 'bool16', 'bool32']
+
 globalVersion = 0
 globalHasChanges = False
 
@@ -33,23 +35,44 @@ def is_integer(n):
         return False
     else:
         return float(n).is_integer()
+    
+def parse_size(tokens, enumlist):
+    sizetokens = ""
+    e = Evaluator()
+    for x in tokens:
+        if is_integer(x.value) or x.value in ['+', '-', '%', '(', ')', '/', '?', ':', '*'] or str(x.value).startswith("0x"):
+            sizetokens += str(x.value)
+        else:
+            if x.value in enumlist:
+                sizetokens += str(enumlist[x.value])
+            else:
+                print("Error: unable to figure out enum value of %s" % x.value)
+                quit()
+    return e(sizetokens)
 
 def parse_field(field, enumlist):
     cl = {}
-    e = Evaluator()
     cl['type'] = field.type.__class__.__name__
+    if (cl['type'] == "Type"):
+        if hasattr(field.type.typename.segments[0], 'name'):
+            cl['kind'] = field.type.typename.segments[0].name
+        else:
+            cl['kind'] = "SM_union%s" % field.type.typename.segments[0].id
+        cl['is_struct'] = (field.type.typename.classkey == 'struct')
+        if field.type.typename.classkey == 'union':
+            cl['is_union'] = True
+    elif (cl['type'] == "Array"):
+        if (field.type.array_of.__class__.__name__ == "Type"):
+            cl['kind'] = field.type.array_of.typename.segments[0].name
+            cl['is_struct'] = (field.type.array_of.typename.classkey == 'struct')
+        elif (field.type.array_of.__class__.__name__ == "Array"):
+            cl['kind'] = field.type.array_of.array_of.typename.segments[0].name
+            cl['is_struct'] = (field.type.array_of.array_of.typename.classkey == 'struct')
     if hasattr(field.type, 'size'):
-        sizetokens = ""
-        for x in field.type.size.tokens:
-            if is_integer(x.value) or x.value in ['+', '-', '%', '(', ')', '/', '?', ':'] or str(x.value).startswith("0x"):
-                sizetokens += str(x.value)
-            else:
-                if x.value in enumlist:
-                    sizetokens += str(enumlist[x.value])
-                else:
-                    print("Error: unable to figure out enum value of %s" % x.value)
-                    quit()
-        cl['size'] = e(sizetokens)
+        cl['size'] = parse_size(field.type.size.tokens, enumlist)
+    if hasattr(field.type, 'array_of'):
+        if hasattr(field.type.array_of, 'size'):
+            cl['size2'] = parse_size(field.type.array_of.size.tokens, enumlist)
     if (field.bits != None):
         cl['bits'] = field.bits
     return(cl)
@@ -90,7 +113,7 @@ for x in contents_new.namespace.classes:
     if hasattr(x.class_decl.typename.segments[0], 'name'):
         classes_new[x.class_decl.typename.segments[0].name] = x
 
-def compareFields(fieldname):
+def compareFields(fieldname, inline):
     # make list of fields
     fields_old = {}
     fields_old_array = []
@@ -99,17 +122,20 @@ def compareFields(fieldname):
         fields_old_array.append(x.name)
 
     # compare
-    print("Comparing %s" % fieldname)
+    print("  " * inline + "Comparing %s" % fieldname)
     for x in classes_new[fieldname].fields:
         oldclass = parse_field(fields_old[x.name], enums_old)
         newclass = parse_field(x, enums_new)
 
-        #print(oldclass, newclass)
+        # print(oldclass, newclass)
 
         if (x.name in fields_old_array):
             fields_old_array.remove(x.name)
             if (x == fields_old[x.name]):
-                # print("%s is identical" % x.name)
+                print("  " * inline + "%s is identical" % x.name)
+                # if identical, check the actual kind to make sure the underlying struct didn't change
+                if newclass['kind'] not in trusted_typedefs and not 'is_union' in newclass: # union support is left to do!
+                    compareFields(newclass['kind'], inline + 1)
                 continue
             # figure out what exactly is different, starting with size
             global globalHasChanges
@@ -119,20 +145,20 @@ def compareFields(fieldname):
                 newsize = newclass['size']
                 if (oldsize != newsize):
                     if (oldsize < newsize):
-                        print("%s was expanded in size from %s to %s" % (x.name, oldsize, newsize))
+                        print("  " * inline + "%s was expanded in size from %s to %s" % (x.name, oldsize, newsize))
                     else:
-                        print("IMPORTANT: %s was truncated in size from %s to %s" % (x.name, oldsize, newsize))
+                        print("  " * inline + "IMPORTANT: %s was truncated in size from %s to %s" % (x.name, oldsize, newsize))
                     continue
-            print("%s is different, but can't identify why!" % x.name)
+            print("  " * inline + "%s is different, but can't identify why!" % x.name)
         else:
-            print("%s is a new field; defaulting values to zero" % x.name)
+            print("  " * inline + "%s is a new field; defaulting values to zero" % x.name)
 
     if len(fields_old_array) > 0:
-        print("The following old fields are not retained: %s" % fields_old_array)
+        print("  " * inline + "The following old fields are not retained: %s" % fields_old_array)
 
-compareFields('SaveBlock2')
-compareFields('SaveBlock1')
-compareFields('PokemonStorage')
+compareFields('SaveBlock2', 0)
+compareFields('SaveBlock1', 0)
+compareFields('PokemonStorage', 0)
 
 # clean up if no changes
 if not globalHasChanges:
