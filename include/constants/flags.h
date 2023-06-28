138290220e9ed234621428f0bed35498d4fed470
Magiya <46344714+mMagiya@users.noreply.github.com>
2023-06-27 20:45:53 -0600

    misc tweaks

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -1217,9 +1217,9 @@
 #define FLAG_ITEM_SAFARI_ZONE_NORTH_EAST_NUGGET                     0x491
 #define FLAG_ITEM_SAFARI_ZONE_SOUTH_EAST_BIG_PEARL                  0x492
 
-#define FLAG_UNUSED_0x493                                           0x493 // Unused Flag
-#define FLAG_UNUSED_0x494                                           0x494 // Unused Flag
-#define FLAG_UNUSED_0x495                                           0x495 // Unused Flag
+#define FLAG_UNUSED_0x493                                           0x493 // Unused Flag - StrangeHouse Gift1
+#define FLAG_UNUSED_0x494                                           0x494 // Unused Flag - StrangeHouse Gift2
+#define FLAG_UNUSED_0x495                                           0x495 // Unused Flag - StrangeHouse Gift3
 #define FLAG_UNUSED_0x496                                           0x496 // Unused Flag
 #define FLAG_UNUSED_0x497                                           0x497 // Unused Flag
 #define FLAG_UNUSED_0x498                                           0x498 // Unused Flag
