a4dec650274e0fff35ad6b4d104bda50031bf60a
Alex <alexthenotes@gmail.com>
2023-08-07 16:52:39 +0200

    remove not needed line

--- data/battle_scripts_1.s
+++ data/battle_scripts_1.s
@@ -5407,7 +5407,6 @@ BattleScript_EffectTeleport:
 .if B_TELEPORT_BEHAVIOR >= GEN_7
 	jumpifbattletype BATTLE_TYPE_TRAINER, BattleScript_EffectBatonPass
 	jumpifside BS_ATTACKER, B_SIDE_PLAYER, BattleScript_EffectBatonPass
-	goto BattleScript_EffectTeleportTryToRunAway
 .else
 	jumpifbattletype BATTLE_TYPE_TRAINER, BattleScript_ButItFailed
 .endif
