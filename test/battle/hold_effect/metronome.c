a8fd7f3f984217e9b7480628c6e626058f3d5bdc
sbird <sbird@no.tld>
2023-08-23 20:54:50 +0200

    fixup: verbose messages

--- test/battle/hold_effect/metronome.c
+++ test/battle/hold_effect/metronome.c
@@ -121,6 +121,11 @@ SINGLE_BATTLE_TEST("Metronome Item counts charging turn of moves for its attacki
         TURN { MOVE(player, MOVE_SOLAR_BEAM); }
         TURN { SKIP_TURN(player); }
     } SCENE {
+        MESSAGE("Wobbuffet used Solar Beam!");
+        MESSAGE("Wobbuffet took in sunlight!");
+        MESSAGE("Foe Wobbuffet used Celebrate!");
+        MESSAGE("Congratulations, 1!");
+        MESSAGE("Wobbuffet used Solar Beam!");
         HP_BAR(opponent, captureDamage: &results[i].damage);
     } FINALLY {
         EXPECT_MUL_EQ(results[0].damage, UQ_4_12(1.2), results[1].damage);
