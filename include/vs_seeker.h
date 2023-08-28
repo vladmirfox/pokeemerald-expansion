f81df905b20ea82ee757d2eadf1235a81fe5bdb0
psf <77138753+pkmnsnfrn@users.noreply.github.com>
2023-08-27 21:24:45 -0700

    Update vs_seeker.h
    https://github.com/rh-hideout/pokeemerald-expansion/pull/3256/files#r1306724158

--- include/vs_seeker.h
+++ include/vs_seeker.h
@@ -9,4 +9,6 @@ void MapResetTrainerRematches(u16 mapGroup, u16 mapNum);
 void ClearRematchMovementByTrainerId(void);
 u16 GetRematchTrainerIdVSSeeker(u16 trainerId);
 
+#define VSSEEKER_RECHARGE_STEPS 100
+
 #endif //GUARD_VS_SEEKER_H
