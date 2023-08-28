a26f9a14d5393e6e31046d21c6fe7fccbfd6cd3c
BLourenco <brandon.lourenco@gmail.com>
2023-08-28 11:49:50 -0400

    Marked tests as TODO for now.

--- test/battle/move_effect/roost.c
+++ test/battle/move_effect/roost.c
@@ -6,33 +6,32 @@ ASSUMPTIONS
     ASSUME(gBattleMoves[MOVE_ROOST].effect == EFFECT_ROOST);
 }
 
-SINGLE_BATTLE_TEST("Roost recovers 50% of the user's Max HP, rounded down, in Gen 4")
-{
-    GIVEN {
-        PLAYER(SPECIES_WOBBUFFET) { HP(1); }
-        OPPONENT(SPECIES_WOBBUFFET);
-    } WHEN {
-        TURN { MOVE(player, MOVE_ROOST); }
-    } SCENE {
-        ANIMATION(ANIM_TYPE_MOVE, MOVE_ROOST, player);
-        HP_BAR(player, hp: 51);
-    }
-}
+TO_DO_BATTLE_TEST("Roost recovers 50% of the user's Max HP, rounded down, in Gen 4");
+// {
+//     GIVEN {
+//         PLAYER(SPECIES_WOBBUFFET) { HP(100); MaxHP(199); }
+//         OPPONENT(SPECIES_WOBBUFFET);
+//     } WHEN {
+//         TURN { MOVE(player, MOVE_ROOST); }
+//     } SCENE {
+//         ANIMATION(ANIM_TYPE_MOVE, MOVE_ROOST, player);
+//         HP_BAR(player, damage: -(199 / 2));
+//     }
+// }
 
-SINGLE_BATTLE_TEST("Roost recovers 50% of the user's Max HP, rounded up, in Gen 5+")
-{
-    GIVEN {
-        PLAYER(SPECIES_WOBBUFFET) { HP(1); }
-        OPPONENT(SPECIES_WOBBUFFET);
-    } WHEN {
-        TURN { MOVE(player, MOVE_ROOST); }
-    } SCENE {
-        ANIMATION(ANIM_TYPE_MOVE, MOVE_ROOST, player);
-        HP_BAR(player, hp: 51);
-    }
-}
+TO_DO_BATTLE_TEST("Roost recovers 50% of the user's Max HP, rounded up, in Gen 5+");
+// {
+//     GIVEN {
+//         PLAYER(SPECIES_WOBBUFFET) { HP(100); MaxHP(199); }
+//         OPPONENT(SPECIES_WOBBUFFET);
+//     } WHEN {
+//         TURN { MOVE(player, MOVE_ROOST); }
+//     } SCENE {
+//         ANIMATION(ANIM_TYPE_MOVE, MOVE_ROOST, player);
+//         HP_BAR(player, damage: -(199 / 2));
+//     }
+// }
 
-// Heal Block tested here or in it's own test file?
 SINGLE_BATTLE_TEST("Roost fails if Heal Block applies")
 {
     GIVEN {
@@ -48,27 +47,13 @@ SINGLE_BATTLE_TEST("Roost fails if Heal Block applies")
     }
 }
 
-SINGLE_BATTLE_TEST("Roost, if used by a _____/Flying-type, removes the user's Flying type until the end of the turn")
-{
-    
-}
+TO_DO_BATTLE_TEST("Roost, if used by a _____/Flying-type, removes the user's Flying type until the end of the turn");
 
-SINGLE_BATTLE_TEST("Roost, if used by a pure Flying-type, leaves the user typeless until the end of the turn in Gen 4")
-{
+TO_DO_BATTLE_TEST("Roost, if used by a pure Flying-type, leaves the user typeless until the end of the turn in Gen 4");
 
-}
+TO_DO_BATTLE_TEST("Roost, if used by a pure Flying-type, changes the user to pure Normal-type until the end of the turn in Gen 5+");
 
-SINGLE_BATTLE_TEST("Roost, if used by a pure Flying-type, changes the user to pure Normal-type until the end of the turn in Gen 5+")
-{
-    
-}
+// Shorter description?
+TO_DO_BATTLE_TEST("Roost, if used by a pure Flying-type and under the effects of Trick-or-Treat or Forest's Curse, changes the user to a Normal-type while retaining the added type until the end of the turn");
 
-// Shorted description?
-SINGLE_BATTLE_TEST("Roost, if used by a pure Flying-type and under the effects of Trick-or-Treat or Forest's Curse, changes the user to a Normal-type while retaining the added type until the end of the turn in Gen 5+")
-{
-    
-}
-
-// Test grounded?
-
-// Test Z-Power effect?
+TO_DO_BATTLE_TEST("Roost, upon making the user lose their Flying-type, also makes the user grounded.");
