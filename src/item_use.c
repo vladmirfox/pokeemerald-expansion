1c33bfc477a334859e454ad58fab9005619c229f
pkmnsnfrn <pkmnsnfrn@gmail.com>
2023-08-30 12:01:52 -0700

    Refactored FieldUseFunc_VsSeeker

--- src/item_use.c
+++ src/item_use.c
@@ -1325,40 +1325,60 @@ void ItemUseOutOfBattle_CannotUse(u8 taskId)
     DisplayDadsAdviceCannotUseItemMessage(taskId, gTasks[taskId].tUsingRegisteredKeyItem);
 }
 
-void FieldUseFunc_VsSeeker(u8 taskId)
+bool32 IsValidLocationForVsSeeker(void)
 {
-    u32 i;
     u16 mapGroup = gSaveBlock1Ptr->location.mapGroup;
     u16 mapNum = gSaveBlock1Ptr->location.mapNum;
-    bool32 isValidMap = (gMapHeader.mapType == MAP_TYPE_ROUTE ||
-            gMapHeader.mapType == MAP_TYPE_TOWN ||
-            gMapHeader.mapType == MAP_TYPE_CITY ||
-            gMapHeader.mapType == MAP_TYPE_OCEAN_ROUTE);
+    u16 mapType = gMapHeader.mapType;
 
-    if (gMapHeader.mapType == MAP_TYPE_INDOOR) {
-        isValidMap = isValidMap && ((mapNum == MAP_NUM(MT_PYRE_SUMMIT)) ||
-            (mapGroup == MAP_NUM(SAFARI_ZONE_NORTH)) ||
-            (mapGroup == MAP_NUM(SAFARI_ZONE_NORTHEAST)) ||
-            (mapGroup == MAP_NUM(SAFARI_ZONE_NORTHWEST)) ||
-            (mapGroup == MAP_NUM(SAFARI_ZONE_SOUTH)) ||
-            (mapGroup == MAP_NUM(SAFARI_ZONE_SOUTHEAST)) ||
-            (mapGroup == MAP_NUM(SAFARI_ZONE_SOUTHWEST)) ||
-            (mapGroup == MAP_NUM(SKY_PILLAR_TOP)) ||
-            (mapGroup == MAP_NUM(SOUTHERN_ISLAND_EXTERIOR)) ||
-            (mapGroup == MAP_NUM(SOUTHERN_ISLAND_INTERIOR)) ||
-            (mapGroup == MAP_NUM(RUSTBORO_CITY_GYM)) ||
-            (mapGroup == MAP_NUM(DEWFORD_TOWN_GYM)) ||
-            (mapGroup == MAP_NUM(MAUVILLE_CITY_GYM)) ||
-            (mapGroup == MAP_NUM(LAVARIDGE_TOWN_GYM_1F)) ||
-            (mapGroup == MAP_NUM(LAVARIDGE_TOWN_GYM_B1F)) ||
-            (mapGroup == MAP_NUM(PETALBURG_CITY_GYM)) ||
-            (mapGroup == MAP_NUM(FORTREE_CITY_GYM)) ||
-            (mapGroup == MAP_NUM(MOSSDEEP_CITY_GYM)) ||
-            (mapGroup == MAP_NUM(SOOTOPOLIS_CITY_GYM_1F)) ||
-            (mapGroup == MAP_NUM(SOOTOPOLIS_CITY_GYM_B1F)));
+    typedef struct {
+        u16 mapGroup;
+        u16 mapNum;
+    } Location;
+
+    u32 i;
+    Location validIndoorLocations[] =
+    {
+        MAP_GROUP(MT_PYRE_SUMMIT), MAP_NUM(MT_PYRE_SUMMIT),
+        MAP_GROUP(SAFARI_ZONE_NORTH), MAP_NUM(SAFARI_ZONE_NORTH),
+        MAP_GROUP(SAFARI_ZONE_NORTHEAST), MAP_NUM(SAFARI_ZONE_NORTHEAST),
+        MAP_GROUP(SAFARI_ZONE_NORTHWEST), MAP_NUM(SAFARI_ZONE_NORTHWEST),
+        MAP_GROUP(SAFARI_ZONE_SOUTH), MAP_NUM(SAFARI_ZONE_SOUTH),
+        MAP_GROUP(SAFARI_ZONE_SOUTHEAST), MAP_NUM(SAFARI_ZONE_SOUTHEAST),
+        MAP_GROUP(SAFARI_ZONE_SOUTHWEST), MAP_NUM(SAFARI_ZONE_SOUTHWEST),
+        MAP_GROUP(SKY_PILLAR_TOP), MAP_NUM(SKY_PILLAR_TOP),
+        MAP_GROUP(SOUTHERN_ISLAND_EXTERIOR), MAP_NUM(SOUTHERN_ISLAND_EXTERIOR),
+        MAP_GROUP(SOUTHERN_ISLAND_INTERIOR), MAP_NUM(SOUTHERN_ISLAND_INTERIOR),
+        MAP_GROUP(RUSTBORO_CITY_GYM), MAP_NUM(RUSTBORO_CITY_GYM),
+        MAP_GROUP(DEWFORD_TOWN_GYM), MAP_NUM(DEWFORD_TOWN_GYM),
+        MAP_GROUP(MAUVILLE_CITY_GYM), MAP_NUM(MAUVILLE_CITY_GYM),
+        MAP_GROUP(LAVARIDGE_TOWN_GYM_1F), MAP_NUM(LAVARIDGE_TOWN_GYM_1F),
+        MAP_GROUP(LAVARIDGE_TOWN_GYM_B1F), MAP_NUM(LAVARIDGE_TOWN_GYM_B1F),
+        MAP_GROUP(PETALBURG_CITY_GYM), MAP_NUM(PETALBURG_CITY_GYM),
+        MAP_GROUP(FORTREE_CITY_GYM), MAP_NUM(FORTREE_CITY_GYM),
+        MAP_GROUP(MOSSDEEP_CITY_GYM), MAP_NUM(MOSSDEEP_CITY_GYM),
+        MAP_GROUP(SOOTOPOLIS_CITY_GYM_1F), MAP_NUM(SOOTOPOLIS_CITY_GYM_1F),
+        MAP_GROUP(SOOTOPOLIS_CITY_GYM_B1F), MAP_NUM(SOOTOPOLIS_CITY_GYM_B1F)
+    };
+
+    if (IsMapTypeOutdoors(mapType))
+        return TRUE;
+
+    if (!IsMapTypeIndoors(mapType))
+        return FALSE;
+
+    for (i = 0; i < ARRAY_COUNT(validIndoorLocations); i++)
+    {
+        if (mapNum == validIndoorLocations[i].mapNum && mapGroup == validIndoorLocations[i].mapGroup)
+            return TRUE;
     }
 
-    if (isValidMap)
+    return FALSE;
+}
+
+void FieldUseFunc_VsSeeker(u8 taskId)
+{
+    if (IsValidLocationForVsSeeker())
     {
         sItemUseOnFieldCB = Task_VsSeeker_0;
         SetUpItemUseOnFieldCallback(taskId);
