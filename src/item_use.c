ca6fe2da0a3944f30ff6cea9ebf2de66dd2c2849
pkmnsnfrn <pkmnsnfrn@gmail.com>
2023-09-01 15:13:20 -0700

    Swapped strings when Exp Share was toggled on or off

--- src/item_use.c
+++ src/item_use.c
@@ -208,19 +208,19 @@ void ItemUseOutOfBattle_ExpShare(u8 taskId)
     FlagToggle(I_EXP_SHARE_FLAG);
     if (!IsGen6ExpShareEnabled())
     {
-        PlaySE(SE_EXP_MAX);
-        if (!gTasks[taskId].data[2]) // to account for pressing select in the overworld
-            DisplayItemMessageOnField(taskId, gText_ExpShareOn, Task_CloseCantUseKeyItemMessage);
-        else
-            DisplayItemMessage(taskId, 1, gText_ExpShareOn, CloseItemMessage);
-    }
-    else
-    {
         PlaySE(SE_PC_OFF);
         if (!gTasks[taskId].data[2]) // to account for pressing select in the overworld
             DisplayItemMessageOnField(taskId, gText_ExpShareOff, Task_CloseCantUseKeyItemMessage);
         else
             DisplayItemMessage(taskId, 1, gText_ExpShareOff, CloseItemMessage);
+    }
+    else
+    {
+        PlaySE(SE_EXP_MAX);
+        if (!gTasks[taskId].data[2]) // to account for pressing select in the overworld
+            DisplayItemMessageOnField(taskId, gText_ExpShareOn, Task_CloseCantUseKeyItemMessage);
+        else
+            DisplayItemMessage(taskId, 1, gText_ExpShareOn, CloseItemMessage);
     }
 #else
     DisplayDadsAdviceCannotUseItemMessage(taskId, gTasks[taskId].tUsingRegisteredKeyItem);
