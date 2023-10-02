b2acf452e9632094945a1d33976f3bcff2d9c60e
fakuzatsu <abyerswilson@gmail.com>
2023-10-02 19:57:28 +0100

    added Time Script back to Debug Menu

--- src/debug.c
+++ src/debug.c
@@ -61,6 +61,7 @@
 #include "constants/species.h"
 #include "constants/weather.h"
 #include "save.h"
+#include "rtc.h"
 
 #if DEBUG_OVERWORLD_MENU == TRUE
 // *******************************
@@ -416,7 +417,7 @@ static const u8 sDebugText_Sound[] =            _("Sound…{CLEAR_TO 110}{RIGHT_
 static const u8 sDebugText_AccessPC[] =         _("Access PC…{CLEAR_TO 110}{RIGHT_ARROW}");
 static const u8 sDebugText_Cancel[] =           _("Cancel");
 // Script menu
-static const u8 sDebugText_Util_Script_1[] =               _("Script 1");
+static const u8 sDebugText_Util_Script_1[] =               _("Change Time");
 static const u8 sDebugText_Util_Script_2[] =               _("Script 2");
 static const u8 sDebugText_Util_Script_3[] =               _("Script 3");
 static const u8 sDebugText_Util_Script_4[] =               _("Script 4");
@@ -1994,6 +1995,10 @@ static void DebugAction_Util_HatchAnEgg(u8 taskId)
 // Actions Scripts
 static void DebugAction_Util_Script_1(u8 taskId)
 {
+    Overworld_ResetStateAfterDigEscRope();
+    ToggleDayNight();
+    RunOnResumeMapScript();
+    DoCurrentWeather();
     Debug_DestroyMenu_Full_Script(taskId, Debug_Script_1);
 }
 static void DebugAction_Util_Script_2(u8 taskId)
