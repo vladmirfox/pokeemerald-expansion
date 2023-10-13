9beca4ff15d28d557daaab209581b8587e42c323
kittenchilly <mario.pikmin.zelda@gmail.com>
2023-10-13 16:33:09 -0500

    Update sandstorm.c

--- test/battle/weather/sandstorm.c
+++ test/battle/weather/sandstorm.c
@@ -35,41 +35,32 @@ SINGLE_BATTLE_TEST("Sandstorm multiplies the special defense of Rock-types by 1.
     }
 }
 
-SINGLE_BATTLE_TEST("Sandstorm damage does not hurt Ground-type Pokémon")
+SINGLE_BATTLE_TEST("Sandstorm damage does not hurt Ground, Rock, and Steel-type Pokémon")
 {
+    u32 mon;
+    PARAMETRIZE { mon = SPECIES_SANDSLASH; }
+    PARAMETRIZE { mon = SPECIES_NOSEPASS; }
+    PARAMETRIZE { mon = SPECIES_REGISTEEL; }
     GIVEN {
         ASSUME(gSpeciesInfo[SPECIES_SANDSLASH].types[0] == TYPE_GROUND);
-        PLAYER(SPECIES_WOBBUFFET);
-        OPPONENT(SPECIES_SANDSLASH);
-    } WHEN {
-        TURN {MOVE(player, MOVE_SANDSTORM);}
-    } SCENE {
-        NOT MESSAGE("Foe Sandslash is buffeted by the sandstorm!");
-    }
-}
-
-SINGLE_BATTLE_TEST("Sandstorm damage does not hurt Rock-type Pokémon")
-{
-    GIVEN {
         ASSUME(gSpeciesInfo[SPECIES_NOSEPASS].types[0] == TYPE_ROCK);
-        PLAYER(SPECIES_WOBBUFFET);
-        OPPONENT(SPECIES_NOSEPASS);
-    } WHEN {
-        TURN {MOVE(player, MOVE_SANDSTORM);}
-    } SCENE {
-        NOT MESSAGE("Foe Nosepass is buffeted by the sandstorm!");
-    }
-}
-
-SINGLE_BATTLE_TEST("Sandstorm damage does not hurt Steel-type Pokémon")
-{
-    GIVEN {
         ASSUME(gSpeciesInfo[SPECIES_REGISTEEL].types[0] == TYPE_STEEL);
         PLAYER(SPECIES_WOBBUFFET);
-        OPPONENT(SPECIES_REGISTEEL);
+        OPPONENT(mon);
     } WHEN {
-        TURN {MOVE(player, MOVE_SANDSTORM);}
+        TURN { MOVE(player, MOVE_SANDSTORM); }
     } SCENE {
-        NOT MESSAGE("Foe Registeel is buffeted by the sandstorm!");
+        switch (mon)
+        {
+        case SPECIES_SANDSLASH:
+            NOT MESSAGE("Foe Sandslash is buffeted by the sandstorm!");
+            break;
+        case SPECIES_NOSEPASS:
+            NOT MESSAGE("Foe Nosepass is buffeted by the sandstorm!");
+            break;
+        case SPECIES_REGISTEEL:
+            NOT MESSAGE("Foe Registeel is buffeted by the sandstorm!");
+            break;
+        }
     }
 }
