43c01dc06eb99b4c10a1bc5264d75307c6f677c2
Martin Griffin <martinrgriffin@gmail.com>
2023-11-09 13:44:15 +0000

    squash! squash! Defines for Expansion version number
    RHH ROM header

--- include/constants/expansion.h
+++ include/constants/expansion.h
@@ -2,8 +2,8 @@
 #define GUARD_CONSTANTS_EXPANSION_H
 
 #define EXPANSION_VERSION_MAJOR 1
-#define EXPANSION_VERSION_MINOR 6
-#define EXPANSION_VERSION_PATCH 1
+#define EXPANSION_VERSION_MINOR 7
+#define EXPANSION_VERSION_PATCH 0
 
 // FALSE if this this version of Expansion is not a tagged commit, i.e.
 // it contains unreleased changes.
