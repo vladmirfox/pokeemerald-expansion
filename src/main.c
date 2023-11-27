3ebdd64f1b6dbe1173ba553e0a42b6cfd970dd81
ethanrdsch <ethanrdsch01@gmail.com>
2023-11-26 19:33:19 -0500

    fix rng

--- src/main.c
+++ src/main.c
@@ -107,6 +107,7 @@ void AgbMain()
     CheckForFlashMemory();
     InitMainCallbacks();
     InitMapMusic();
+    SeedRngAndSetTrainerId();
 #ifdef BUGFIX
     SeedRngWithRtc(); // see comment at SeedRngWithRtc definition below
 #endif
@@ -215,9 +216,11 @@ void StartTimer1(void)
 
 void SeedRngAndSetTrainerId(void)
 {
-    u16 val = REG_TM1CNT_L;
+    u32 seed = RtcGetMinuteCount();
+    u16 val = RtcGetMinuteCount();
+    seed = (seed >> 16) ^ (seed & 0xFFFF);
+    SeedRng(seed);
     SeedRng(val);
-    REG_TM1CNT_H = 0;
     sTrainerId = val;
 }
 
