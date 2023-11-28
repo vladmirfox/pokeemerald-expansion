de87f215840ffa16ba99c292b51e220c49d3263f
ethanrdsch <ethanrdsch01@gmail.com>
2023-11-28 10:59:17 -0500

    surf dismount ground fx bug fix

--- src/field_player_avatar.c
+++ src/field_player_avatar.c
@@ -1678,6 +1678,7 @@ static void Task_WaitStopSurfing(u8 taskId)
         gPlayerAvatar.preventStep = FALSE;
         UnlockPlayerFieldControls();
         DestroySprite(&gSprites[playerObjEvent->fieldEffectSpriteId]);
+        playerObjEvent->triggerGroundEffectsOnMove = TRUE;
         DestroyTask(taskId);
     }
 }
