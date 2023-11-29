ce70db0346516d2f50065e04f7ea630eda78fd53
ethanrdsch <ethanrdsch01@gmail.com>
2023-11-29 10:18:14 -0500

    faster save message

--- src/start_menu.c
+++ src/start_menu.c
@@ -1220,7 +1220,7 @@ static u8 SaveFileExistsCallback(void)
     }
     else
     {
-        ShowSaveMessage(gText_AlreadySavedFile, SaveConfirmOverwriteCallback);
+        sSaveDialogCallback = SaveSavingMessageCallback;
     }
 
     return SAVE_IN_PROGRESS;
