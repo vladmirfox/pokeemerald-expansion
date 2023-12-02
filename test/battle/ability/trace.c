2bb77414e209d1a5a1f870dd46eb37dec2581b25
LOuroboros <lunosouroboros@gmail.com>
2023-12-02 18:40:48 -0300

    Removed 2 of the 3 'Trace on opponent's switch-in' tests

--- test/battle/ability/trace.c
+++ test/battle/ability/trace.c
@@ -28,7 +28,7 @@ SINGLE_BATTLE_TEST("Trace copies opponents ability on switch-in")
     }
 }
 
-SINGLE_BATTLE_TEST("Trace copies opponents ability on switch-in even if opponent switched in at the same time - 1")
+SINGLE_BATTLE_TEST("Trace copies opponents ability on switch-in even if opponent switched in at the same time")
 {
     GIVEN {
         PLAYER(SPECIES_WOBBUFFET)
@@ -39,38 +39,6 @@ SINGLE_BATTLE_TEST("Trace copies opponents ability on switch-in even if opponent
         TURN { MOVE(player, MOVE_MISTY_EXPLOSION); SEND_OUT(opponent, 1); SEND_OUT(player, 1); }
     } SCENE {
         ANIMATION(ANIM_TYPE_MOVE, MOVE_MISTY_EXPLOSION);
-        ABILITY_POPUP(player, ABILITY_TRACE);
-        MESSAGE("Ralts TRACED Foe Torchic's Blaze!");
-    }
-}
-
-SINGLE_BATTLE_TEST("Trace copies opponents ability on switch-in even if opponent switched in at the same time - 2")
-{
-    GIVEN {
-        PLAYER(SPECIES_WOBBUFFET)
-        PLAYER(SPECIES_RALTS) { Ability(ABILITY_TRACE); }
-        OPPONENT(SPECIES_TREECKO) { HP(1); }
-        OPPONENT(SPECIES_TORCHIC) { Ability(ABILITY_BLAZE); }
-    } WHEN {
-        TURN { MOVE(player, MOVE_VOLT_SWITCH); SEND_OUT(opponent, 1); SEND_OUT(player, 1); }
-    } SCENE {
-        ANIMATION(ANIM_TYPE_MOVE, MOVE_VOLT_SWITCH);
-        ABILITY_POPUP(player, ABILITY_TRACE);
-        MESSAGE("Ralts TRACED Foe Torchic's Blaze!");
-    }
-}
-
-SINGLE_BATTLE_TEST("Trace copies opponents ability on switch-in even if opponent switched in at the same time - 3")
-{
-    GIVEN {
-        PLAYER(SPECIES_WOBBUFFET) { Item(ITEM_EJECT_PACK); }
-        PLAYER(SPECIES_RALTS) { Ability(ABILITY_TRACE); }
-        OPPONENT(SPECIES_TREECKO) { HP(1); }
-        OPPONENT(SPECIES_TORCHIC) { Ability(ABILITY_BLAZE); }
-    } WHEN {
-        TURN { MOVE(player, MOVE_OVERHEAT); SEND_OUT(opponent, 1); SEND_OUT(player, 1); }
-    } SCENE {
-        ANIMATION(ANIM_TYPE_MOVE, MOVE_OVERHEAT);
         ABILITY_POPUP(player, ABILITY_TRACE);
         MESSAGE("Ralts TRACED Foe Torchic's Blaze!");
     }
