a14e442e412d5a50258e6909eb738f3ee8a80b1b
Martin Griffin <martinrgriffin@gmail.com>
2023-12-11 10:30:40 +0000

    Test form change targets have appropriate species flags

--- test/species.c
+++ test/species.c
@@ -44,3 +44,35 @@ TEST("Form change tables contain only forms in the form species ID table")
         EXPECT(formSpeciesIdTable[j] != FORM_SPECIES_END);
     }
 }
+
+TEST("Form change targets have the appropriate species flags")
+{
+    u32 i;
+    u32 species = SPECIES_NONE;
+    for (i = 0; i < NUM_SPECIES; i++)
+    {
+        if (gSpeciesInfo[i].formChangeTable) PARAMETRIZE { species = i; }
+    }
+
+    const struct FormChange *formChangeTable = gSpeciesInfo[species].formChangeTable;
+    for (i = 0; formChangeTable[i].method != FORM_CHANGE_TERMINATOR; i++)
+    {
+        const struct SpeciesInfo *targetSpeciesInfo = &gSpeciesInfo[formChangeTable[i].targetSpecies];
+        switch (formChangeTable[i].method)
+        {
+        case FORM_CHANGE_BATTLE_MEGA_EVOLUTION_ITEM:
+        case FORM_CHANGE_BATTLE_MEGA_EVOLUTION_MOVE:
+            EXPECT(targetSpeciesInfo->isMegaEvolution);
+            break;
+        case FORM_CHANGE_BATTLE_PRIMAL_REVERSION:
+            EXPECT(targetSpeciesInfo->isPrimalReversion);
+            break;
+        case FORM_CHANGE_BATTLE_ULTRA_BURST:
+            EXPECT(targetSpeciesInfo->isUltraBurst);
+            break;
+        case FORM_CHANGE_BATTLE_GIGANTAMAX:
+            EXPECT(targetSpeciesInfo->isGigantamax);
+            break;
+       }
+    }
+}
