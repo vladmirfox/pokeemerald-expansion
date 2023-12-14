72e2bb39ca5cb166611ec06183496f284e46786a
ethanrdsch <ethanrdsch01@gmail.com>
2023-12-14 10:01:54 -0500

    make double slap 18 power

--- src/data/battle_moves.h
+++ src/data/battle_moves.h
@@ -53,7 +53,7 @@ const struct BattleMove gBattleMoves[MOVES_COUNT_Z] =
     [MOVE_DOUBLE_SLAP] =
     {
         .effect = EFFECT_MULTI_HIT,
-        .power = 20,
+        .power = 18,
         .type = TYPE_NORMAL,
         .accuracy = 100,
         .pp = 10,
