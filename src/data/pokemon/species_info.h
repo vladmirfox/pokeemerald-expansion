602728e79913923df966d269573a3c8a137eb2d9
Eduardo Quezada <eduardo602002@gmail.com>
2023-12-14 15:06:58 -0300

    Removed gen_custom file to allow usage of MISC_INFO macros

--- src/data/pokemon/species_info.h
+++ src/data/pokemon/species_info.h
@@ -226,7 +226,6 @@ const u8 gOgerponCornerstoneMaskPokedexText[] = _(
 
 const struct SpeciesInfo gSpeciesInfo[] =
 {
-    /*
     [SPECIES_NONE] =
     {
         .speciesName = _("??????????"),
@@ -255,7 +254,7 @@ const struct SpeciesInfo gSpeciesInfo[] =
         ICON(QuestionMark, 0),
         LEARNSETS(None),
     },
-    */
+
     #include "species_info/gen_1.h"
     #include "species_info/gen_2.h"
     #include "species_info/gen_3.h"
@@ -265,7 +264,6 @@ const struct SpeciesInfo gSpeciesInfo[] =
     #include "species_info/gen_7.h"
     #include "species_info/gen_8.h"
     #include "species_info/gen_9.h"
-    #include "species_info/gen_custom.h"
 
     [SPECIES_EGG] =
     {
@@ -277,5 +275,68 @@ const struct SpeciesInfo gSpeciesInfo[] =
         .palette = gMonPalette_Egg,
         .shinyPalette = gMonPalette_Egg,
         ICON(Egg, 1),
-    }
+    },
+
+    /* You may add any custom species below this point based on the following structure: */
+
+    /*
+    [SPECIES_NONE] =
+    {
+        .baseHP        = 1,
+        .baseAttack    = 1,
+        .baseDefense   = 1,
+        .baseSpeed     = 1,
+        .baseSpAttack  = 1,
+        .baseSpDefense = 1,
+        .types = { TYPE_MYSTERY, TYPE_MYSTERY },
+        .catchRate = 255,
+        .expYield = 67,
+        .evYield_HP = 1,
+        .evYield_Defense = 1,
+        .evYield_SpDefense = 1,
+        .genderRatio = PERCENT_FEMALE(50),
+        .eggCycles = 20,
+        .friendship = STANDARD_FRIENDSHIP,
+        .growthRate = GROWTH_MEDIUM_FAST,
+        .eggGroups = { EGG_GROUP_NO_EGGS_DISCOVERED, EGG_GROUP_NO_EGGS_DISCOVERED },
+        .abilities = { ABILITY_NONE, ABILITY_CURSED_BODY, ABILITY_DAMP },
+        .bodyColor = BODY_COLOR_BLACK,
+        .speciesName = _("??????????"),
+        .cryId = CRY_NONE,
+        .natDexNum = NATIONAL_DEX_NONE,
+        .categoryName = _("Unknown"),
+        .height = 0,
+        .weight = 0,
+        .description = COMPOUND_STRING(
+            "This is a newly discovered Pokémon.\n"
+            "It is currently under investigation.\n"
+            "No detailed information is available\n"
+            "at this time."),
+        .pokemonScale = 256,
+        .pokemonOffset = 0,
+        .trainerScale = 256,
+        .trainerOffset = 0,
+        FRONT_PIC(CircledQuestionMark, 64, 64),
+        //FRONT_PIC_FEMALE(CircledQuestionMark, 64, 64),
+        .frontPicYOffset = 0,
+        .frontAnimFrames = sAnims_None,
+        //.frontAnimId = ANIM_V_SQUISH_AND_BOUNCE,
+        .enemyMonElevation = 0,
+        BACK_PIC(CircledQuestionMark, 64, 64),
+        //BACK_PIC_FEMALE(CircledQuestionMark, 64, 64),
+        .backPicYOffset = 7,
+        .backAnimId = BACK_ANIM_NONE,
+        PALETTES(CircledQuestionMark),
+        //PALETTE_FEMALE(CircledQuestionMark),
+        ICON(QuestionMark, 0),
+        //ICON_FEMALE(QuestionMark, 1),
+        //.footprint = gMonFootprint_None,
+        LEARNSETS(None),
+        .evolutions = EVOLUTION({EVO_LEVEL, 100, SPECIES_NONE},
+                                {EVO_ITEM, ITEM_MOOMOO_MILK, SPECIES_NONE}),
+        //.formSpeciesIdTable = sNoneFormSpeciesIdTable,
+        //.formChangeTable = sNoneFormChangeTable,
+        .allPerfectIVs = TRUE,
+    },
+    */
 };
