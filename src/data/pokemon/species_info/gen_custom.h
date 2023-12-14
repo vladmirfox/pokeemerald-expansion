bd016522b17c0e0fd8c54d0c38ea0b4415062872
Eduardo Quezada <eduardo602002@gmail.com>
2023-12-14 14:26:18 -0300

    Separated gSpeciesInfo by generation

---
+++ src/data/pokemon/species_info/gen_custom.h
@@ -0,0 +1,69 @@
+#ifdef __INTELLISENSE__
+const struct SpeciesInfo gSpeciesInfoGenCustom[] =
+{
+#endif
+
+    /* You may add your custom species here based on the following structure:
+    [SPECIES_NONE] =
+    {
+        .baseHP        = 1,
+        .baseAttack    = 1,
+        .baseDefense   = 1,
+        .baseSpeed     = 1,
+        .baseSpAttack  = 1,
+        .baseSpDefense = 1,
+        .types = { TYPE_MYSTERY, TYPE_MYSTERY },
+        .catchRate = 255,
+        .expYield = 67,
+        .evYield_HP = 1,
+        .evYield_Defense = 1,
+        .evYield_SpDefense = 1,
+        .genderRatio = PERCENT_FEMALE(50),
+        .eggCycles = 20,
+        .friendship = STANDARD_FRIENDSHIP,
+        .growthRate = GROWTH_MEDIUM_FAST,
+        .eggGroups = { EGG_GROUP_NO_EGGS_DISCOVERED, EGG_GROUP_NO_EGGS_DISCOVERED },
+        .abilities = { ABILITY_NONE, ABILITY_CURSED_BODY, ABILITY_DAMP },
+        .bodyColor = BODY_COLOR_BLACK,
+        .speciesName = _("??????????"),
+        .cryId = CRY_NONE,
+        .natDexNum = NATIONAL_DEX_NONE,
+        .categoryName = _("Unknown"),
+        .height = 0,
+        .weight = 0,
+        .description = COMPOUND_STRING(
+            "This is a newly discovered Pokémon.\n"
+            "It is currently under investigation.\n"
+            "No detailed information is available\n"
+            "at this time."),
+        .pokemonScale = 256,
+        .pokemonOffset = 0,
+        .trainerScale = 256,
+        .trainerOffset = 0,
+        FRONT_PIC(CircledQuestionMark, 64, 64),
+        //FRONT_PIC_FEMALE(CircledQuestionMark, 64, 64),
+        .frontPicYOffset = 0,
+        .frontAnimFrames = sAnims_None,
+        //.frontAnimId = ANIM_V_SQUISH_AND_BOUNCE,
+        .enemyMonElevation = 0,
+        BACK_PIC(CircledQuestionMark, 64, 64),
+        //BACK_PIC_FEMALE(CircledQuestionMark, 64, 64),
+        .backPicYOffset = 7,
+        .backAnimId = BACK_ANIM_NONE,
+        PALETTES(CircledQuestionMark),
+        //PALETTE_FEMALE(CircledQuestionMark),
+        ICON(QuestionMark, 0),
+        //ICON_FEMALE(QuestionMark, 1),
+        //.footprint = gMonFootprint_None,
+        LEARNSETS(None),
+        .evolutions = EVOLUTION({EVO_LEVEL, 100, SPECIES_NONE},
+                                {EVO_ITEM, ITEM_MOOMOO_MILK, SPECIES_NONE}),
+        //.formSpeciesIdTable = sNoneFormSpeciesIdTable,
+        //.formChangeTable = sNoneFormChangeTable,
+        .allPerfectIVs = TRUE,
+    },
+    */
+
+#ifdef __INTELLISENSE__
+};
+#endif
