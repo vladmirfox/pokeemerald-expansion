c40c5a02310062f2429039271b7e48637c24e67c
Ninjdai <65647523+Ninjdai1@users.noreply.github.com>
2023-12-29 13:55:05 +0100

    Rename numSpecies and movesCount to match the constants

--- src/rom_header_rhh.c
+++ src/rom_header_rhh.c
@@ -16,8 +16,8 @@ struct RHHRomHeader
     /*0x07*/ u8 expansionVersionMinor;
     /*0x08*/ u8 expansionVersionPatch;
     /*0x09*/ u8 expansionVersionFlags;
-    /*0x0A*/ int expansionMovesCount;
-    /*0x0B*/ int expansionMonCount;
+    /*0x0A*/ int movesCount;
+    /*0x0B*/ int numSpecies;
 };
 
 static const struct RHHRomHeader sRHHRomHeader =
@@ -27,6 +27,6 @@ static const struct RHHRomHeader sRHHRomHeader =
     .expansionVersionMinor = EXPANSION_VERSION_MINOR,
     .expansionVersionPatch = EXPANSION_VERSION_PATCH,
     .expansionVersionFlags = (EXPANSION_TAGGED_RELEASE << 0),
-    .expansionMovesCount = MOVES_COUNT,
-    .expansionMonCount = NUM_SPECIES,
+    .movesCount = MOVES_COUNT,
+    .numSpecies = NUM_SPECIES,
 };
