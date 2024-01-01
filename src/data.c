689589a40fe1d4b939ae22c13c138abaeff4dce6
Frank <fdeblasio1004@gmail.com>
2024-01-01 17:14:02 -0500

    Moved trainer classes into battle_main.c

--- src/data.c
+++ src/data.c
@@ -220,6 +220,5 @@ const union AnimCmd *const gAnims_MonPic[MAX_MON_PIC_FRAMES] =
 };
 
 #include "data/trainer_parties.h"
-#include "data/trainer_classes.h"
 #include "data/trainers.h"
 #include "data/text/move_names.h"
