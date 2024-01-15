1322564062a6788a8f3eac2ca47d7a7b85bd4505
pkmnsnfrn <pkmnsnfrn@gmail.com>
2024-01-15 13:22:55 -0800

    Created ITEM_NAME_PLURAL_LENGTH

--- include/item.h
+++ include/item.h
@@ -15,7 +15,7 @@ struct Item
     const u8 *description;
     const u8 *effect;
     u8 name[ITEM_NAME_LENGTH];
-    u8 pluralName[ITEM_NAME_LENGTH + 2]; // allow room for potential -ies suffix
+    u8 pluralName[ITEM_NAME_PLURAL_LENGTH];
     u8 holdEffect;
     u8 holdEffectParam;
     u8 importance;
