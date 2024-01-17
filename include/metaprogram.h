49e420261c42a57b235fff9871764e75151ff44f
Nephrite <thechurchofcage@gmail.com>
2024-01-17 15:12:46 +0900

    Expanded DEFAULT
    Because why not?

--- include/metaprogram.h
+++ include/metaprogram.h
@@ -60,8 +60,10 @@
 #define R_FOR_EACH_WITH_(macro, args, a, ...) INVOKE_WITH(macro, args, a) __VA_OPT__(R_FOR_EACH_WITH_P PARENS (macro, args, __VA_ARGS__))
 #define R_FOR_EACH_WITH_P() R_FOR_EACH_WITH_
 
-/* Picks the first VA_ARG if it exists, otherwise returns a default value */
+/* Picks the xth VA_ARG if it exists, otherwise returns a default value */
 #define DEFAULT(_default, ...) FIRST(__VA_OPT__(__VA_ARGS__, ) _default)
 #define DEFAULT_2(_default, ...) DEFAULT(_default __VA_OPT__(, SECOND(__VA_ARGS__)))
+#define DEFAULT_3(_default, ...) DEFAULT(_default __VA_OPT__(, THIRD(__VA_ARGS__)))
+#define DEFAULT_4(_default, ...) DEFAULT(_default __VA_OPT__(, FOURTH(__VA_ARGS__)))
 
 #endif
