72f016ba1e584bf8cbe181f07189d1189657008f
tertu marybig <flameshadowxeroshin@gmail.com>
2024-01-23 13:36:48 -0600

    Re-disable NDEBUG

--- src/randomizer.c
+++ src/randomizer.c
@@ -244,10 +244,12 @@ static inline u16 GetSpeciesGroup(const struct SpeciesTable* table, u16 species)
         return GROUP_INVALID;
     species -= 1;
     groupEntry = table->speciesGroups[table->speciesIndex[species]];
+
     #ifndef NDEBUG
         MgbaPrintf(MGBA_LOG_INFO, "GetSpeciesGroup: input %lu species %lu group %lu",
             (unsigned long)species+1, (unsigned long)groupEntry.species, (unsigned long)groupEntry.group);
     #endif
+
     return groupEntry.group;
 
 }
