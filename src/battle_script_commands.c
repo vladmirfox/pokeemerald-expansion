87d4bbec402197524af9ab93aec412e1ad8a16dc
ZnogyroP <z-nogyrop@hotmail.ca>
2024-01-31 09:14:24 -0500

    Update battle_script_commands.c

--- src/battle_script_commands.c
+++ src/battle_script_commands.c
@@ -16530,9 +16530,9 @@ void BS_TryUpperHand(void)
     NATIVE_ARGS(const u8 *failInstr);
 
     if (GetBattlerTurnOrderNum(gBattlerAttacker) > GetBattlerTurnOrderNum(gBattlerTarget) 
-	|| gChosenMoveByBattler[gBattlerTarget] == MOVE_NONE 
-	|| IS_MOVE_STATUS(gChosenMoveByBattler[gBattlerTarget]) 
-	|| GetChosenMovePriority(gBattlerTarget) < 1 || GetChosenMovePriority(gBattlerTarget) > 3) // Fails if priority is less than 1 or greater than 3, if target already moved, or if using a status move
+     || gChosenMoveByBattler[gBattlerTarget] == MOVE_NONE 
+     || IS_MOVE_STATUS(gChosenMoveByBattler[gBattlerTarget]) 
+     || GetChosenMovePriority(gBattlerTarget) < 1 || GetChosenMovePriority(gBattlerTarget) > 3) // Fails if priority is less than 1 or greater than 3, if target already moved, or if using a status 
         gBattlescriptCurrInstr = cmd->failInstr;
     else
         gBattlescriptCurrInstr = cmd->nextInstr;
