004132dc555520d3256faf61c98f8a1035b5a522
purrfectdoodle <purrfectdoodle@gmail.com>
2024-02-05 23:27:00 +0100

    Chii town map & tileset

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1076,3 +1076,5 @@ EventScript_VsSeekerChargingDone::
 	.include "data/maps/Sunrise_Village_Beach/scripts.inc"
 
 	.include "data/maps/GinkoWoods/scripts.inc"
+
+	.include "data/maps/ChiiTown/scripts.inc"
