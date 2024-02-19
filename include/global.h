e838a7d13689e08398ff40353a7c8ba3b98ecd12
Eduardo Quezada <eduardo602002@gmail.com>
2024-02-19 10:31:10 -0300

    Added FREE_EXTRA_SEEN_FLAGS to Pokedex struct

--- include/global.h
+++ include/global.h
@@ -190,7 +190,9 @@ struct Pokedex
     /*0x04*/ u32 unownPersonality; // set when you first see Unown
     /*0x08*/ u32 spindaPersonality; // set when you first see Spinda
     /*0x0C*/ u32 unknown3;
+#if FREE_EXTRA_SEEN_FLAGS == FALSE
     /*0x10*/ u8 filler[0x68]; // Previously Dex Flags, feel free to remove.
+#endif //FREE_EXTRA_SEEN_FLAGS
 };
 
 struct PokemonJumpRecords
