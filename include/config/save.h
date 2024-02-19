c506f7da4137bd5439fcf7b7e5c812b245d62d66
Eduardo Quezada <eduardo602002@gmail.com>
2024-02-19 10:48:41 -0300

    Fixed SaveBlock1 comment (please squash)

--- include/config/save.h
+++ include/config/save.h
@@ -9,7 +9,7 @@
 #define FREE_ENIGMA_BERRY                   FALSE   // Frees up E-Reader Enigma Berry data (52 bytes).
 #define FREE_LINK_BATTLE_RECORDS            FALSE   // Frees up link battle record data (88 bytes).
 #define FREE_MYSTERY_GIFT                   FALSE   // Frees up Mystery Gift data (876 bytes).
-                                            // SaveBlock1 total: 2516 bytes
+                                            // SaveBlock1 total: 2464 bytes
 // SaveBlock2 configs
 #define FREE_BATTLE_TOWER_E_READER          FALSE   // Frees up Battle Tower E-Reader data (188 bytes).
 #define FREE_POKEMON_JUMP                   FALSE   // Frees up Pokémon Jump data (16 bytes).
