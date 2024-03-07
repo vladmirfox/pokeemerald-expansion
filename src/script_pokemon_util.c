7b140bab49b33f3e1bb16a0565ce1b18215475f3
LOuroboros <lunosouroboros@gmail.com>
2024-03-07 19:30:20 -0300

    And made it respect P_FLAG_FORCE_NO_SHINY too

--- src/script_pokemon_util.c
+++ src/script_pokemon_util.c
@@ -315,6 +315,8 @@ u32 ScriptGiveMonParameterized(u16 species, u8 level, u16 item, u8 ball, u8 natu
     // shininess
     if (P_FLAG_FORCE_SHINY != 0 && FlagGet(P_FLAG_FORCE_SHINY))
         isShiny = TRUE;
+    else if (P_FLAG_FORCE_NO_SHINY != 0 && FlagGet(P_FLAG_FORCE_NO_SHINY))
+        isShiny = FALSE;
     SetMonData(&mon, MON_DATA_IS_SHINY, &isShiny);
 
     // gigantamax factor
