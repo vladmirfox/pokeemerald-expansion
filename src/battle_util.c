90cf2f026401e1b2839206079b1d8169a35a70d5
Alex <alexthenotes@gmail.com>
2024-03-12 00:40:41 +0100

    Couple things for 1.8 release

--- src/battle_util.c
+++ src/battle_util.c
@@ -8581,7 +8581,7 @@ static inline u32 CalcMoveBasePower(u32 move, u32 battlerAtk, u32 battlerDef, u3
             basePower *= 2;
         break;
     case EFFECT_VARY_POWER_BASED_ON_HP:
-        basePower = gMovesInfo[move].argument * gBattleMons[battlerDef].hp / gBattleMons[battlerDef].maxHP;
+        basePower = gMovesInfo[move].power * gBattleMons[battlerDef].hp / gBattleMons[battlerDef].maxHP;
         break;
     case EFFECT_ASSURANCE:
         if (gProtectStructs[battlerDef].physicalDmg != 0 || gProtectStructs[battlerDef].specialDmg != 0 || gProtectStructs[battlerDef].confusionSelfDmg)
