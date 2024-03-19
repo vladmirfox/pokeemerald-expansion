219317ab70d4a087050aeab24344ec1d441cd951
Russell Maggs <russell.maggs.1@gmail.com>
2024-03-19 18:40:08 +1300

    fix python script not selecting correct encoding

--- tools/learnset_helpers/teachable.py
+++ tools/learnset_helpers/teachable.py
@@ -25,7 +25,7 @@ if len(incs_to_check) == 0: # disabled if no jsons present
     quit()
 
 for file in incs_to_check:
-    with open(file, 'r') as f2:
+    with open(file, 'r', encoding="utf-8") as f2:
         raw = f2.read()
     if 'special ChooseMonForMoveTutor' in raw:
         for x in re.findall('setvar VAR_0x8005, (MOVE_.*)', raw):
