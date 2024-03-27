09ccb41f795b3fb155faadf63cc4e7d12718dfe0
purrfectdoodle <purrfectdoodle@gmail.com>
2024-03-27 23:08:20 +0100

    Windswept route / Windycape

--- src/data/object_events/object_event_graphics_info.h
+++ src/data/object_events/object_event_graphics_info.h
@@ -152,6 +152,7 @@ const struct ObjectEventGraphicsInfo gObjectEventGraphicsInfo_Twin = {
 
 const struct ObjectEventGraphicsInfo gObjectEventGraphicsInfo_Boy1 = {
     .tileTag = TAG_NONE,
+    // .paletteTag = OBJ_EVENT_PAL_TAG_NPC_1,
     .paletteTag = OBJ_EVENT_PAL_TAG_BOY_1,
     .reflectionPaletteTag = OBJ_EVENT_PAL_TAG_NONE,
     .size = 512,
