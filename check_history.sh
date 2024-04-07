f14182fbae00da5685fddc1313f54b1fbf7c91f8
Eduardo Quezada <eduardo602002@gmail.com>
2024-04-07 08:40:45 -0400

    pokemon_expansion -> pokeemerald-expansion

--- check_history.sh
+++ check_history.sh
@@ -21,7 +21,7 @@ fi
 
 if [ $has_git -ne 1 ]
 then
-    echo -e "\033[0;31mfatal: \033[0m\033[1;33mgit was not found. You will be unable to use version control, update pokemon_expansion, or use feature branches. To use version control, install \`git\` and clone the repository instead of using \"Download Zip\" on GitHub. Run \`touch .histignore\` to ignore this and continue anyways.\033[0m"
+    echo -e "\033[0;31mfatal: \033[0m\033[1;33mgit was not found. You will be unable to use version control, update pokeemerald-expansion, or use feature branches. To use version control, install \`git\` and clone the repository instead of using \"Download Zip\" on GitHub. Run \`touch .histignore\` to ignore this and continue anyways.\033[0m"
     exit 1
 fi
 
@@ -29,7 +29,7 @@ if [ "$has_hist" ]
 then
    exit 0
 else
-   echo -e "\033[0;31mfatal: \033[0m\033[1;33mno git history found. You will be unable to use version control, update pokemon_expansion, or use feature branches. To use version control, use \`git\` to clone the repository instead of using \"Download Zip\" on GitHub. Run \`touch .histignore\` to ignore this and continue anyways.\033[0m"
+   echo -e "\033[0;31mfatal: \033[0m\033[1;33mno git history found. You will be unable to use version control, update pokeemerald-expansion, or use feature branches. To use version control, use \`git\` to clone the repository instead of using \"Download Zip\" on GitHub. Run \`touch .histignore\` to ignore this and continue anyways.\033[0m"
    exit 1
 fi
 
