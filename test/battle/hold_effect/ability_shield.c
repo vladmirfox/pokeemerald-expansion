7493db33e0058e42ded41413b3cd60c52d55e20e
Alex <alexthenotes@gmail.com>
2024-04-14 18:23:39 +0200

    test was wrong

--- test/battle/hold_effect/ability_shield.c
+++ test/battle/hold_effect/ability_shield.c
@@ -8,25 +8,25 @@ ASSUMPTIONS
 
 SINGLE_BATTLE_TEST("Ability Shield prevents Neutralizing Gas")
 {
-    u32 ability;
+    u32 item;
 
-    PARAMETRIZE { ability = ABILITY_DROUGHT; }
-    PARAMETRIZE { ability = ABILITY_WHITE_SMOKE; }
+    PARAMETRIZE { item = ITEM_ABILITY_SHIELD; }
+    PARAMETRIZE { item = ITEM_NONE; }
 
     GIVEN {
-        PLAYER(SPECIES_TORKOAL) { Ability(ability); Item(ITEM_ABILITY_SHIELD); }
+        PLAYER(SPECIES_TORKOAL) { Ability(ABILITY_DROUGHT); Item(item); }
         OPPONENT(SPECIES_KOFFING) { Ability(ABILITY_NEUTRALIZING_GAS); }
     } WHEN {
         TURN { }
     } SCENE {
         ABILITY_POPUP(opponent, ABILITY_NEUTRALIZING_GAS);
         MESSAGE("Neutralizing Gas filled the area!");
-        if (ability == ABILITY_DROUGHT) {
-            ABILITY_POPUP(player, ability);
+        if (item == ITEM_ABILITY_SHIELD) {
+            ABILITY_POPUP(player, ABILITY_DROUGHT);
             MESSAGE("Torkoal's Drought intensified the sun's rays!");
         } else {
             NONE_OF {
-                ABILITY_POPUP(player, ability);
+                ABILITY_POPUP(player, ABILITY_DROUGHT);
                 MESSAGE("Torkoal's Drought intensified the sun's rays!");
             }
         }
