5010d48a351df80e3b058e24c4fe469e9169687b
AgustinGDLV <agustingomezdelavega@gmail.com>
2024-04-24 19:18:14 -0700

    updated CanTerastallize to be disabled if B_FLAG_TERA_ORB_CHARGED undefined

--- src/battle_terastal.c
+++ src/battle_terastal.c
@@ -47,10 +47,10 @@ bool32 CanTerastallize(u32 battler)
     u32 holdEffect = GetBattlerHoldEffect(battler, FALSE);
 
     // Check if Player has Tera Orb and has charge.
-    if (B_FLAG_TERA_ORB_CHARGED != 0
-        && (battler == B_POSITION_PLAYER_LEFT || (!(gBattleTypeFlags & BATTLE_TYPE_MULTI) && battler == B_POSITION_PLAYER_RIGHT))
+    if (B_FLAG_TERA_ORB_CHARGED == 0
+        || ((battler == B_POSITION_PLAYER_LEFT || (!(gBattleTypeFlags & BATTLE_TYPE_MULTI) && battler == B_POSITION_PLAYER_RIGHT))
         && !(CheckBagHasItem(ITEM_TERA_ORB, 1)
-        && FlagGet(B_FLAG_TERA_ORB_CHARGED)))
+        && FlagGet(B_FLAG_TERA_ORB_CHARGED))))
     {
         return FALSE;
     }
