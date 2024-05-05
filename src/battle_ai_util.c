c84d198e66f4414d1525a5f62e2b9858d70fa17b
Eduardo Quezada <eduardo602002@gmail.com>
2024-05-04 21:49:21 -0400

    Fixed battle_ai_util.c whitespace

--- src/battle_ai_util.c
+++ src/battle_ai_util.c
@@ -439,9 +439,9 @@ bool32 IsDamageMoveUnusable(u32 move, u32 battlerAtk, u32 battlerDef)
             return TRUE;
         break;
     case EFFECT_HIT_SET_REMOVE_TERRAIN:
-	if (!(gFieldStatuses & STATUS_FIELD_TERRAIN_ANY))
-	    return TRUE;
-	break;
+        if (!(gFieldStatuses & STATUS_FIELD_TERRAIN_ANY))
+            return TRUE;
+        break;
     }
 
     return FALSE;
