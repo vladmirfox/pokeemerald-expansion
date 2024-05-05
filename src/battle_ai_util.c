718b8893ab5e8c639c625aa4debd2fa90144608c
Eduardo Quezada <eduardo602002@gmail.com>
2024-05-05 09:48:52 -0400

    Update src/battle_ai_util.c
    Co-authored-by: Alex <93446519+AlexOn1ine@users.noreply.github.com>

--- src/battle_ai_util.c
+++ src/battle_ai_util.c
@@ -439,7 +439,7 @@ bool32 IsDamageMoveUnusable(u32 move, u32 battlerAtk, u32 battlerDef)
             return TRUE;
         break;
     case EFFECT_HIT_SET_REMOVE_TERRAIN:
-        if (!(gFieldStatuses & STATUS_FIELD_TERRAIN_ANY) && (gMovesInfo[move].argument == ARG_TRY_REMOVE_TERRAIN_HIT || gMovesInfo[move].argument == ARG_TRY_REMOVE_TERRAIN_FAIL))
+        if (!(gFieldStatuses & STATUS_FIELD_TERRAIN_ANY) && gMovesInfo[move].argument == ARG_TRY_REMOVE_TERRAIN_FAIL)
             return TRUE;
         break;
     }
