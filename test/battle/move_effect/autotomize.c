dbff47ca120bd134cb4d11ca988d9bd5abe0195e
Bassoonian <iasperbassoonian@gmail.com>
2024-05-15 20:35:23 +0200

    Update test/battle/move_effect/autotomize.c

--- test/battle/move_effect/autotomize.c
+++ test/battle/move_effect/autotomize.c
@@ -1,7 +1,7 @@
 #include "global.h"
 #include "test/battle.h"
 
-TO_DO_BATTLE_TEST("Autotomize increases speed by 2 stages");
+TO_DO_BATTLE_TEST("Autotomize increases Speed by 2 stages");
 TO_DO_BATTLE_TEST("Autotomize decreases weight by 100kg (220 lbs.)");
 TO_DO_BATTLE_TEST("Autotomize can be used multiple times to decrease weight each time");
 TO_DO_BATTLE_TEST("Autotomize cannot decrease weight below 0.1kg (0.2 lbs)");
