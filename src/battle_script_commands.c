3d45ffe2434eecc15e0a280cacee7fa732685032
Alex <93446519+AlexOn1ine@users.noreply.github.com>
2024-05-22 00:36:22 +0200

    BS_SetGlaiveRush formatting
    Please merge my ocd induced change.

--- src/battle_script_commands.c
+++ src/battle_script_commands.c
@@ -16435,9 +16435,7 @@ void BS_TrySetOctolock(void)
 void BS_SetGlaiveRush(void)
 {
     NATIVE_ARGS();
-
     gStatuses4[gBattlerAttacker] |= STATUS4_GLAIVE_RUSH;
-
     gBattlescriptCurrInstr = cmd->nextInstr;
 }
 
