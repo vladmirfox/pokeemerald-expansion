fa79be9ae97d2b46f81709a0b596f5cf7aacf888
Alex <93446519+AlexOn1ine@users.noreply.github.com>
2024-05-31 22:16:46 +0200

    Update src/battle_main.c

--- src/battle_main.c
+++ src/battle_main.c
@@ -3677,7 +3677,6 @@ const u8* FaintClearSetData(u32 battler)
     gBattleStruct->zmove.active = FALSE;
     gBattleStruct->zmove.toBeUsed[battler] = MOVE_NONE;
     gBattleStruct->zmove.effect = EFFECT_HIT;
-	
     // Clear Dynamax data
     UndoDynamax(battler);	
 	
