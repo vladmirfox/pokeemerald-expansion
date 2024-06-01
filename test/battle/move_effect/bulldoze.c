239d019e89b966f6364e2c273512f7ef27c90fb1
Eduardo Quezada <eduardo602002@gmail.com>
2024-06-01 08:29:19 -0400

    Removed unused bulldoze effect file

--- test/battle/move_effect/bulldoze.c
+++
@@ -1,5 +0,0 @@
-#include "global.h"
-#include "test/battle.h"
-
-TO_DO_BATTLE_TEST("Bulldoze lowers the speed of every battler hit");
-TO_DO_BATTLE_TEST("Bulldoze's damage is halved when Grassy Terrain is in effect lowers the speed of every battler hit");
