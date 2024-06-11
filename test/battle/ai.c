ac336b146351397cdfa8ea663cb77532b2e8d9bb
sneed <devilduke1995@gmail.com>
2024-06-11 16:09:13 +0300

    Parametrize test AI flags

--- test/battle/ai.c
+++ test/battle/ai.c
@@ -715,8 +715,12 @@ AI_SINGLE_BATTLE_TEST("AI calculates guaranteed criticals and detects critical i
     }
 }
 
-AI_SINGLE_BATTLE_TEST("AI uses a guaranteed KO move instead of a 50% KO high crit move")
+AI_SINGLE_BATTLE_TEST("AI uses a guaranteed KO move instead of the move with the highest expected damage")
 {
+    u32 flags;
+
+    PARAMETRIZE { flags = AI_FLAG_CHECK_BAD_MOVE | AI_FLAG_CHECK_VIABILITY; }
+    PARAMETRIZE { flags = AI_FLAG_CHECK_BAD_MOVE | AI_FLAG_CHECK_VIABILITY | AI_FLAG_TRY_TO_FAINT; }
 
     GIVEN {
         ASSUME(gMovesInfo[MOVE_SLASH].criticalHitStage == 1);
@@ -724,11 +728,19 @@ AI_SINGLE_BATTLE_TEST("AI uses a guaranteed KO move instead of a 50% KO high cri
         ASSUME(gMovesInfo[MOVE_STRENGTH].power == 80);
         ASSUME(gMovesInfo[MOVE_SLASH].type == gMovesInfo[MOVE_STRENGTH].type);
         ASSUME(gMovesInfo[MOVE_SLASH].category == gMovesInfo[MOVE_STRENGTH].category);
-        AI_FLAGS(AI_FLAG_CHECK_BAD_MOVE | AI_FLAG_CHECK_VIABILITY | AI_FLAG_TRY_TO_FAINT);
+        AI_FLAGS(flags);
         PLAYER(SPECIES_WOBBUFFET) { HP(225); }
         OPPONENT(SPECIES_ABSOL) { Ability(ABILITY_SUPER_LUCK); Moves(MOVE_SLASH, MOVE_STRENGTH); }
     } WHEN {
         TURN { EXPECT_MOVE(opponent, MOVE_SLASH); }
-        TURN { EXPECT_MOVE(opponent, MOVE_STRENGTH); }
+        if (flags & AI_FLAG_TRY_TO_FAINT)
+            TURN { EXPECT_MOVE(opponent, MOVE_STRENGTH); }
+        else
+            TURN { EXPECT_MOVE(opponent, MOVE_SLASH); }
+    } SCENE {
+        if (flags & AI_FLAG_TRY_TO_FAINT)
+            MESSAGE("Wobbuffet fainted!");
+        else
+            NOT MESSAGE("Wobbuffet fainted!");
     }
 }
