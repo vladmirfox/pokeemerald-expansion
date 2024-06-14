762239da6022902a4f7f794a53a002de2a4271d6
purrfectdoodle <purrfectdoodle@gmail.com>
2024-06-14 13:58:02 +0200

    Faster text + faster held item effect anim + faster HP drain

--- src/battle_interface.c
+++ src/battle_interface.c
@@ -2637,7 +2637,8 @@ s32 MoveBattleBar(u8 battlerId, u8 healthboxSpriteId, u8 whichBar, u8 unused)
                     gBattleSpritesDataPtr->battleBars[battlerId].oldValue,
                     gBattleSpritesDataPtr->battleBars[battlerId].receivedValue,
                     &gBattleSpritesDataPtr->battleBars[battlerId].currValue,
-                    B_HEALTHBAR_PIXELS / 8, hpFraction);
+                    // TODO EVA check if this isn't too quick for HP drain
+                    B_HEALTHBAR_PIXELS / 8, max(gBattleSpritesDataPtr->battleBars[battlerId].maxValue / 32, 1));
     }
     else // exp bar
     {
