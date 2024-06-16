f221927d9c357f4aa9fffa201614e02b0afc8c7d
sneed <devilduke1995@gmail.com>
2024-06-16 11:56:41 +0300

    add another test

--- test/battle/ability/moxie.c
+++ test/battle/ability/moxie.c
@@ -89,3 +89,33 @@ SINGLE_BATTLE_TEST("Moxie does not trigger when already at maximum Attack stage"
         EXPECT_EQ(player->statStages[STAT_ATK], MAX_STAT_STAGE);
     }
 }
+
+DOUBLE_BATTLE_TEST("Moxie does not increase damage done by the same move that causes another Pokemon to faint")
+{
+    s16 damage[2];
+
+    ASSUME(gMovesInfo[MOVE_EARTHQUAKE].target == MOVE_TARGET_FOES_AND_ALLY);
+
+    KNOWN_FAILING; // Requires simultaneous damage implementation
+    GIVEN {
+        PLAYER(SPECIES_SALAMENCE) { Ability(ABILITY_MOXIE); }
+        PLAYER(SPECIES_ABRA) { HP(1); }
+        OPPONENT(SPECIES_GLALIE);
+        OPPONENT(SPECIES_GLALIE);
+        OPPONENT(SPECIES_ABRA);
+    } WHEN {
+        TURN { MOVE(playerLeft, MOVE_EARTHQUAKE); }
+    } SCENE {
+        ANIMATION(ANIM_TYPE_MOVE, MOVE_EARTHQUAKE, playerLeft);
+        HP_BAR(opponentLeft, captureDamage: &damage[0]);
+        HP_BAR(playerRight);
+        MESSAGE("Abra fainted!");
+        ABILITY_POPUP(playerLeft, ABILITY_MOXIE);
+        ANIMATION(ANIM_TYPE_GENERAL, B_ANIM_STATS_CHANGE, playerLeft);
+        MESSAGE("Salamence's Moxie raised its Attack!");
+        HP_BAR(opponentRight, captureDamage: &damage[1]);
+    } THEN {
+        EXPECT_EQ(playerLeft->statStages[STAT_ATK], DEFAULT_STAT_STAGE + 1);
+        EXPECT_EQ(damage[0], damage[1]);
+    }
+}
