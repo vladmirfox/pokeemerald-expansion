9fd896c856e1ce4712897d75c25505e55824ef2e
sneed <devilduke1995@gmail.com>
2024-06-20 23:24:12 +0300

    Replace three instances of int i with one int turn

--- test/battle/move_effect/fury_cutter.c
+++ test/battle/move_effect/fury_cutter.c
@@ -10,27 +10,22 @@ ASSUMPTIONS
 SINGLE_BATTLE_TEST("Fury Cutter power doubles with each use, up to 3 times")
 {
     s16 damage[4];
+    int turn;
 
     GIVEN {
         PLAYER(SPECIES_CROBAT);
         OPPONENT(SPECIES_LINOONE) { HP(900); }
     } WHEN {
-        int i;
-
-        for (i = 0; i < 4; i++)
+        for (turn = 0; turn < 4; turn++)
             TURN { MOVE(player, MOVE_FURY_CUTTER); }
     } SCENE {
-        int i;
-
-        for (i = 0; i < 4; i++) {
+        for (turn = 0; turn < 4; turn++) {
             ANIMATION(ANIM_TYPE_MOVE, MOVE_FURY_CUTTER, player);
-            HP_BAR(opponent, captureDamage: &damage[i]);
+            HP_BAR(opponent, captureDamage: &damage[turn]);
         }
     } THEN {
-        int i;
-
-        for (i = 0; i < 2; i++)
-            EXPECT_MUL_EQ(damage[i], UQ_4_12(2.0), damage[i + 1]);
+        for (turn = 0; turn < 2; turn++)
+            EXPECT_MUL_EQ(damage[turn], UQ_4_12(2.0), damage[turn + 1]);
         EXPECT_EQ(damage[2], damage[3]);
     }
 }
