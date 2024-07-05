f1355ba536ce2843dc75196e853dd2dc5341f0b4
pkmnsnfrn <pkmnsnfrn@gmail.com>
2024-07-05 16:29:39 -0700

    Changed ALTERED_TIME_RATIO to GEN_LATEST

--- include/config/overworld.h
+++ include/config/overworld.h
@@ -91,6 +91,6 @@
 //Time
 #define OW_TIMES_OF_DAY                 GEN_LATEST // Different generations have the times of day change at different times.
 #define OW_USE_FAKE_RTC                 FALSE      // When TRUE, seconds on the in-clock will only advance once every 60 playTimeVBlanks (every 60 frames).
-#define OW_ALTERED_TIME_RATIO           GEN_8_PLA  // In GEN_8_PLA, the time in game moves forward 60 seconds for every second in the RTC. In GEN_9, it is 20 seconds. This has no effect if OW_USE_FAKE_RTC is FALSE.
+#define OW_ALTERED_TIME_RATIO           GEN_LATEST // In GEN_8_PLA, the time in game moves forward 60 seconds for every second in the RTC. In GEN_9, it is 20 seconds. This has no effect if OW_USE_FAKE_RTC is FALSE.
 
 #endif // GUARD_CONFIG_OVERWORLD_H
