39e62dd9cb968a55b46d0e61bd6d5d358830edd5
Alex <93446519+AlexOn1ine@users.noreply.github.com>
2024-07-09 13:50:12 +0200

    Update src/battle_anim_effects_3.c

--- src/battle_anim_effects_3.c
+++ src/battle_anim_effects_3.c
@@ -2340,7 +2340,7 @@ void AnimTask_HideSwapSprite(u8 taskId)
     {
     case 0:
         gTasks[taskId].data[11] = gSprites[spriteId].x; // Save battler position
-        gSprites[spriteId].x = -64; // hide it from screen to avoid the blip/glitch effect when swapping the sprite. For you, Alex
+        gSprites[spriteId].x = -64; // hide it from screen to avoid the blip/glitch effect when swapping the sprite.
         gTasks[taskId].data[10] = gBattleAnimArgs[0];
         gTasks[taskId].data[0]++;
         break;
