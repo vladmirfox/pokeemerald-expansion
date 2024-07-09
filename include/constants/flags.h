b4e31897a838deca4fa0af269c9e3a74632b0a2d
Anrich Tait <anrichjtait@gmail.com>
2024-07-09 13:47:05 +0200

    Changed the givemon script flags and object ids to match the object in each map so they disappear after giving the mon

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -42,10 +42,10 @@
 #define TEMP_FLAGS_END   FLAG_TEMP_1F
 #define NUM_TEMP_FLAGS   (TEMP_FLAGS_END - TEMP_FLAGS_START + 1)
 
-#define FLAG_UNUSED_0x020    0x20 // Unused Flag
-#define FLAG_UNUSED_0x021    0x21 // Unused Flag
-#define FLAG_UNUSED_0x022    0x22 // Unused Flag
-#define FLAG_UNUSED_0x023    0x23 // Unused Flag
+#define FLAG_UNUSED_0x020    0x20 // Rustboro givemon
+#define FLAG_UNUSED_0x021    0x21 // Dewford givemon
+#define FLAG_UNUSED_0x022    0x22 // Mauville givemon
+#define FLAG_UNUSED_0x023    0x23 // Lavaridge givemon
 #define FLAG_UNUSED_0x024    0x24 // Unused Flag
 #define FLAG_UNUSED_0x025    0x25 // Unused Flag
 #define FLAG_UNUSED_0x026    0x26 // Unused Flag
