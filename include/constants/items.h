70829c2aa66219a807ad498288ec991a23a405ce
vladmirfox <lbrown1895@gmail.com>
2024-07-26 19:48:50 -0400

    uped ITEMS_COUNT

--- include/constants/items.h
+++ include/constants/items.h
@@ -1007,7 +1007,7 @@
 // HOPO BERRY
 // LEGEND PLATE
 
-#define ITEMS_COUNT 832
+#define ITEMS_COUNT 838
 #define ITEM_FIELD_ARROW ITEMS_COUNT
 
 // A special item id associated with "Cancel"/"Exit" etc. in a list of items or decorations
