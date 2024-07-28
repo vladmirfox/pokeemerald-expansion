79e4633f789cc877374ce27a3c2ecc50cc585d5f
pkmnsnfrn <pkmnsnfrn@gmail.com>
2024-07-28 09:24:56 -0700

    Updated config desc per https://github.com/rh-hideout/pokeemerald-expansion/pull/5050/files\#r1694220835

--- include/config/overworld.h
+++ include/config/overworld.h
@@ -97,6 +97,6 @@
 // Pokémon Center
 #define OW_IGNORE_EGGS_ON_HEAL                GEN_LATEST    // In Gen 4+, the nurse in the Pokémon Center does not heal Eggs on healing machine.
 #define OW_UNION_DISABLE_CHECK                FALSE         // When TRUE, the nurse does not inform the player if there is a trainer waiting in the Union Room or at Trainer Hill. Entering a Pokémon Center will be faster as a result.
-#define OW_UNION_CONNECTED_ONLY_CHECK         FALSE         // When TRUE, the Union Room check only occurs unless the Wireless Adapter is connected.
+#define OW_UNION_CONNECTED_ONLY_CHECK         FALSE         // When TRUE, the Union Room check only occurs unless the Wireless Adapter is connected. This configwill have no effect is OW_UNION_DISABLE_CHECK is TRUE.
 
 #endif // GUARD_CONFIG_OVERWORLD_H
