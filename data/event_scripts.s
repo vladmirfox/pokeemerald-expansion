fccb03ba02443cee97d0bab751abd7c827ba9562
vladmirfox <lbrown1895@gmail.com>
2024-07-27 21:00:43 -0400

    updates

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1076,3 +1076,5 @@ EventScript_VsSeekerChargingDone::
 	.include "data/maps/Wyrmshire/scripts.inc"
 
 	.include "data/maps/ProvingTrialEntrance/scripts.inc"
+
+	.include "data/maps/ProvingTrialA1/scripts.inc"
--- include/constants/map_groups.h
+++ include/constants/map_groups.h
@@ -65,6 +65,7 @@
 #define MAP_UNDERWATER_ROUTE125    (56 | (0 << 8))
 #define MAP_WYRMSHIRE              (57 | (0 << 8))
 #define MAP_PROVING_TRIAL_ENTRANCE (58 | (0 << 8))
+#define MAP_PROVING_TRIAL_A1       (59 | (0 << 8))
 
 // gMapGroup_IndoorLittleroot
 #define MAP_LITTLEROOT_TOWN_BRENDANS_HOUSE_1F    (0 | (1 << 8))
