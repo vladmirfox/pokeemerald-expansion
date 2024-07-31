e1bb4f71ec7938107ffe2a1d1bafb7bc6f4f3e8b
AlexOn1ine <alexthenotes@gmail.com>
2024-07-31 11:53:08 +0200

    restore trainer party

--- src/data/trainer_parties.h
+++ src/data/trainer_parties.h
@@ -5945,14 +5945,6 @@ static const struct TrainerMon sParty_GinaAndMia1[] = {
     {
     .lvl = 6,
     .species = SPECIES_LOTAD,
-    },
-    {
-    .lvl = 6,
-    .species = SPECIES_SEEDOT,
-    },
-    {
-    .lvl = 6,
-    .species = SPECIES_LOTAD,
     }
 };
 
