2469e9557d68be1efe0fb5aaccc325d7a639fd99
vladmirfox <lbrown1895@gmail.com>
2024-08-07 20:25:48 -0400

    PTattempt

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -456,7 +456,7 @@
 #define FLAG_REMATCH_DRAKE                   0x1A8
 #define FLAG_REMATCH_WALLACE                 0x1A9
 
-#define FLAG_UNUSED_0x1AA                    0x1AA // Unused Flag
+#define FLAG_PROVINGTRIAL                    0x1AA // Used to check if you have done Starter event.
 #define FLAG_UNUSED_0x1AB                    0x1AB // Unused Flag
 
 #define FLAG_DEFEATED_DEOXYS                 0x1AC
