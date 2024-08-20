8673c08e1c425e08d6fa576bbc8eeef3227394eb
Eduardo Quezada <eduardo602002@gmail.com>
2024-08-20 16:47:43 -0400

    Moved ability and move effect tests to folders by letter

--- test/battle/ability/a/as_one.c
+++ test/battle/ability/a/as_one.c
@@ -1,7 +1,7 @@
 #include "global.h"
 #include "test/battle.h"
 
-// Tests for the individual ability effects are handled in the following times:
+// Tests for the individual ability effects are handled in the following files:
 // - Unnerve: test/battle/ability/unnerve.c
-// - Chilling Neigh: test/battle/ability/chilling_neigh.c
+// - Chilling Neigh: test/battle/ability/moxie.c
 // - Grim Neigh: test/battle/ability/grim_neigh.c
