df7ebcb41f46bc9b7d2752d45b08868ed3855322
SarnPoke <77281351+SarnPoke@users.noreply.github.com>
2024-08-23 23:15:45 +0200

    Add Speed Check
    Checks if mon is faster than its Anger Point partner when scoring.

--- src/battle_ai_main.c
+++ src/battle_ai_main.c
@@ -2871,6 +2871,7 @@ static s32 AI_DoubleBattle(u32 battlerAtk, u32 battlerDef, u32 move, s32 score)
             case ABILITY_ANGER_POINT:
                 if (gMovesInfo[move].alwaysCriticalHit == TRUE 
                     && BattlerStatCanRise(battlerAtkPartner, atkPartnerAbility, STAT_ATK)
+                    && AI_IsFaster(battlerAtk, battlerAtkPartner, move)
                     && !CanIndexMoveFaintTarget(battlerAtk, battlerAtkPartner, AI_THINKING_STRUCT->movesetIndex, 1))
                 {
                     RETURN_SCORE_PLUS(GOOD_EFFECT);
