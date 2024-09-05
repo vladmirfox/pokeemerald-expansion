60f2e57e9dc89b7bc1b13199f65bc76a1f84dd69
ghoulslash <pokevoyager0@gmail.com>
2024-09-04 20:54:23 -0400

    GetBattlerForBattleScript in trainerslideout

--- src/battle_script_commands.c
+++ src/battle_script_commands.c
@@ -15647,7 +15647,7 @@ static void Cmd_trainerslideout(void)
 {
     CMD_ARGS(u8 position);
 
-    u32 battler = GetBattlerAtPosition(cmd->position);
+    u32 battler = GetBattlerForBattleScript(cmd->position);
     BtlController_EmitTrainerSlideBack(battler, BUFFER_A);
     MarkBattlerForControllerExec(battler);
 
