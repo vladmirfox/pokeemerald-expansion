749f69e3fbe52109c28869511b82d2db1556c8d0
pkmnsnfrn <pkmnsnfrn@gmail.com>
2024-09-06 23:17:56 -0700

    Added constants for difficulty

--- include/difficulty.h
+++ include/difficulty.h
@@ -1,22 +1,13 @@
 #ifndef GUARD_DIFFICULTY_H
 #define GUARD_DIFFICULTY_H
 
+#include "constants/difficulty.h"
+
 u32 GetCurrentDifficultyLevel(void);
 u32 GetBattlePartnerDifficultyLevel(u16);
 u32 GetTrainerDifficultyLevel(u16);
 void Script_IncreaseDifficulty(void);
 void Script_DecreaseDifficulty(void);
 void Script_SetDifficulty(u32);
-
-enum DifficultyLevel
-{
-    DIFFICULTY_EASY,
-    DIFFICULTY_NORMAL, //If you rename this, the word "Normal" in fprint_trainers must be replaced with the new difficulty name.
-    DIFFICULTY_HARD,
-    DIFFICULTY_COUNT,
-};
-
-#define DIFFICULTY_MIN 0
-#define DIFFICULTY_MAX (DIFFICULTY_COUNT - 1)
 
 #endif // GUARD_DIFFICULTY_H
