25858c8402ed6297813af253d4b4f3781ac729d7
Rose <83477269+RSilicon@users.noreply.github.com>
2024-09-18 14:18:31 -0400

    Simplify HP Logic
    Fun fact: By setting maxHP for Shedinja earlier in the code, we do not need to worry about it for calculating HP later down the line.
    
    What only matters is: currentHP only increases if the maxHP is greater than previous maxHP (currentHP can never go down), and currentHP is always at most maxHP.
    
    currentHP cannot be more than newMaxHP anyway unless it already was prior.

--- src/pokemon.c
+++ src/pokemon.c
@@ -1812,29 +1812,15 @@ void CalculateMonStats(struct Pokemon *mon)
     CALC_STAT(baseSpAttack, spAttackIV, spAttackEV, STAT_SPATK, MON_DATA_SPATK)
     CALC_STAT(baseSpDefense, spDefenseIV, spDefenseEV, STAT_SPDEF, MON_DATA_SPDEF)
 
-    if (species == SPECIES_SHEDINJA)
-    {
-        if (currentHP != 0 || oldMaxHP == 0)
-            currentHP = 1;
-        else
-            return;
-    }
+    if (currentHP == 0 && oldMaxHP != 0)
+        return;
+
+    if (newMaxHP > oldMaxHP)
+        currentHP += newMaxHP - oldMaxHP;
+    else if (currentHP > newMaxHP)
+        currentHP = newMaxHP;
     else
-    {
-        if (currentHP == 0 && oldMaxHP == 0)
-            currentHP = newMaxHP;
-        else if (currentHP != 0)
-        {
-            if (newMaxHP > oldMaxHP)
-                currentHP += newMaxHP - oldMaxHP;
-            if (currentHP <= 0)
-                currentHP = 1;
-            if (currentHP > newMaxHP)
-                currentHP = newMaxHP;
-        }
-        else
-            return;
-    }
+        return;
 
     SetMonData(mon, MON_DATA_HP, &currentHP);
 }
