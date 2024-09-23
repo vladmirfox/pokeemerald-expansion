0fd79b1a838c22887f2dc2e0ddfc0df3472e068c
kittenchilly <mario.pikmin.zelda@gmail.com>
2024-09-23 10:33:56 -0500

    Merge level_caps and ev_caps into one caps file

--- include/config/caps.h
+++ include/config/caps.h
@@ -1,5 +1,5 @@
-#ifndef GUARD_CONFIG_LEVEL_CAP_H
-#define GUARD_CONFIG_LEVEL_CAP_H
+#ifndef GUARD_CONFIG_CAPS_H
+#define GUARD_CONFIG_CAPS_H
 
 // Constants
 #define EXP_CAP_NONE                    0 // Regular behavior, no level caps are applied
@@ -10,6 +10,11 @@
 #define LEVEL_CAP_FLAG_LIST             1 // Level cap is chosen according to the first unset flag in `sLevelCapFlagMap`
 #define LEVEL_CAP_VARIABLE              2 // Level cap is chosen according to the contents of the event variable specified by B_LEVEL_CAP_VARIABLE
 
+#define EV_CAP_NONE                     0 // Regular behavior, no EV caps are applied
+#define EV_CAP_FLAG_LIST                1 // EV cap is chosen according to the first unset flag in `sEVCapFlagMap`
+#define EV_CAP_VARIABLE                 2 // EV cap is chosen according to the contents of the event variable specified by B_EV_CAP_VARIABLE
+#define EV_CAP_NO_GAIN                  3 // No EVs can be gained
+
 // Configs
 #define B_EXP_CAP_TYPE                  EXP_CAP_NONE   // [EXP_CAP_NONE, EXP_CAP_HARD, EXP_CAP_SOFT] choose the type of level cap to apply
 #define B_LEVEL_CAP_TYPE                LEVEL_CAP_NONE // [LEVEL_CAP_NONE, LEVEL_CAP_FLAG_LIST, LEVEL_CAP_VARIABLE] choose the method to derive the level cap
@@ -18,4 +23,9 @@
 #define B_RARE_CANDY_CAP                FALSE // If set to true, Rare Candies can't be used to go over the level cap
 #define B_LEVEL_CAP_EXP_UP              FALSE // If set to true, mons under level cap will receive more experience
 
-#endif /* GUARD_CONFIG_LEVEL_CAP_H */
+#define B_EV_CAP_TYPE                   EV_CAP_NONE   // [EV_CAP_NONE, EV_CAP_FLAG_LIST, EV_CAP_VARIABLE, EV_CAP_NO_GAIN] choose the type of EV cap to apply#define B_EV_CAP_VARIABLE               12 // event variable used to derive EV cap if B_EV_CAP_TYPE is set to EV_CAP_VARIABLE
+#define B_EV_CAP_VARIABLE               8 // event variable used to derive EV cap if B_EV_CAP_TYPE is set to EV_CAP_VARIABLE
+
+#define B_EV_ITEMS_CAP                  FALSE // If set to true, EV-boosting items can't be used to go over the EV cap
+
+#endif /* GUARD_CONFIG_CAPS_H */
