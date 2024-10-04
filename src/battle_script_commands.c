7cf6781b51bc64365c9a3fb292d5c7760be1aab8
ghoulslash <41651341+ghoulslash@users.noreply.github.com>
2024-10-03 21:18:02 -0400

    Update src/battle_script_commands.c
    Co-authored-by: Bassoonian <iasperbassoonian@gmail.com>

--- src/battle_script_commands.c
+++ src/battle_script_commands.c
@@ -11476,7 +11476,7 @@ static void Cmd_setmiracleeye(void)
 bool8 UproarWakeUpCheck(u8 battler)
 {
     s32 i;
-    bool32 hasSoundproof = (GetBattlerAbility(battler) == ABILITY_SOUNDPROOF && B_UPROAR_IGNORE_SOUNDPROOF < GEN_5);
+    bool32 hasSoundproof = (B_UPROAR_IGNORE_SOUNDPROOF < GEN_5 && GetBattlerAbility(battler) == ABILITY_SOUNDPROOF);
 
     for (i = 0; i < gBattlersCount; i++)
     {
