10da84bdf10f97f6ca9b9eed61a827b6b9132d53
Eduardo Quezada <eduardo602002@gmail.com>
2024-11-10 22:06:31 -0300

    Reorder female fields

--- migration_scripts/1.10/species_info/main.py
+++ migration_scripts/1.10/species_info/main.py
@@ -17,6 +17,9 @@ label_renames = [
     ["KalosCap", "Kalos", "KALOS_CAP", "KALOS"],
     ["Partner", "Starter", r"PARTNER\] =\n    \{\n {8}\.baseHP * = 45,", r"STARTER] =\n    {\n        .baseHP        = 45,"],        # Hacky way of avoiding conflict between Partner and Partner Cap Pikachu
     ["PartnerCap", "Partner", r"PARTNER_CAP\] =\n    \{\n {8}\.baseHP * = 35,", r"PARTNER] =\n    {\n        .baseHP        = 35,"], # Hacky way of avoiding conflict between Partner and Partner Cap Pikachu
+    ["CombatBreed", "Combat", "COMBAT_BREED", "COMBAT"],
+    ["BlazeBreed", "Blaze", "BLAZE_BREED", "BLAZE"],
+    ["AquaBreed", "Aqua", "AQUA_BREED", "AQUA"],
 ]
 
 if not os.path.exists("Makefile"):
@@ -321,6 +324,103 @@ for root, dirs, files in os.walk(main_dir):
             species_content = re.sub(r"s(\w+)" + form_labels[0] + r"(LevelUp|Teachable|EggMove)Learnset", r"s\1" + form_labels[1] + r"\2Learnset", species_content)
             species_content = re.sub(r"g(\w+)" + form_labels[0] + r"PokedexText", r"g\1" + form_labels[1] + r"PokedexText", species_content)
             species_content = re.sub(r"SPECIES_(\w+)_" + form_labels[2] + r"", r"SPECIES_\1_" + form_labels[3] + r"", species_content)
+
+        '''
+        .frontPicFemale = gMonFrontPic_JellicentF,
+        .frontPicSize = MON_COORDS_SIZE(64, 64),
+        .frontPicSizeFemale = MON_COORDS_SIZE(64, 64),
+        .frontPicYOffset = 3,
+        .frontAnimFrames = ANIM_FRAMES(
+            ANIMCMD_FRAME(0, 8),
+            ANIMCMD_FRAME(1, 30),
+            ANIMCMD_FRAME(0, 8),
+            ANIMCMD_FRAME(1, 30),
+            ANIMCMD_FRAME(0, 8),
+        ),
+        .frontAnimId = ANIM_V_SQUISH_AND_BOUNCE,
+        .backPic = gMonBackPic_Jellicent,
+        .backPicFemale = gMonBackPic_JellicentF,
+        .backPicSize = MON_COORDS_SIZE(64, 48),
+        .backPicSizeFemale = MON_COORDS_SIZE(64, 48),
+        .backPicYOffset = 10,
+        .backAnimId = BACK_ANIM_GROW_STUTTER,
+        .palette = gMonPalette_Jellicent,
+        .paletteFemale = gMonPalette_JellicentF,
+        .shinyPalette = gMonShinyPalette_Jellicent,
+        .iconSprite = gMonIcon_Jellicent,
+        .iconPalIndex = 0,
+#if P_GENDER_DIFFERENCES
+        .shinyPaletteFemale = gMonShinyPalette_JellicentF,
+        .iconSpriteFemale = gMonIcon_JellicentF,
+        .iconPalIndexFemale = 1,
+#endif //P_GENDER_DIFFERENCES
+        FOOTPRINT(Jellicent)
+        '''
+        
+        # Reorder Female fields
+        '''
+        species_content = re.sub(r"    \[SPECIES_(\w+)\] =\n    \{\n(((?!    \[SPECIES_).*\n){1,}?)(        \.iconPalIndexFemale = \w+,\n|)        FOOTPRINT\((\w+)\)",
+                                 r"    [SPECIES_\1] =\n    {\n\2#if P_GENDER_DIFFERENCES\n\4#endif //P_GENDER_DIFFERENCES\n        FOOTPRINT(\5)", species_content)
+        species_content = re.sub(r"    \[SPECIES_(\w+)\] =\n    \{\n(((?!(    \[SPECIES_)|\.iconSpriteFemale).*\n){1,}?)(        \.iconSpriteFemale = \w+,\n|) {8}\.iconPalIndex = (\w+),\n#if P_GENDER_DIFFERENCES\n",
+                                 r"    [SPECIES_\1] =\n    {\n\2        .iconPalIndex = \6,\n#if P_GENDER_DIFFERENCES\n\5", species_content)
+        species_content = re.sub(r"    \[SPECIES_(\w+)\] =\n    \{\n(((?!(    \[SPECIES_)|\.shinyPaletteFemale).*\n){1,}?)(        \.shinyPaletteFemale = \w+,\n|) {8}\.iconSprite = (\w+),\n#if P_GENDER_DIFFERENCES\n",
+                                 r"    [SPECIES_\1] =\n    {\n\2        .iconSprite = \6,\n#if P_GENDER_DIFFERENCES\n\5", species_content)
+
+        .frontPicSize = MON_COORDS_SIZE(64, 64),
+        .frontPicSizeFemale = MON_COORDS_SIZE(64, 64),
+        .frontPicYOffset = 3,
+        .frontAnimFrames = sAnims_Venusaur,
+        '''
+        species_content = re.sub(r" {8}\.frontPic = (.*),"
+                               + r"(\n {8}\.frontPicFemale = .*,|)"
+                               + r"\n {8}\.frontPicSize = (.*),"
+                               + r"(\n {8}\.frontPicSizeFemale = MON_COORDS_SIZE\(\w+, \w+\),|)"
+                               + r"(\n {8}\.frontPicYOffset = .*,|)"
+                               + r"\n {8}\.frontAnimFrames = (.*),"
+                               + r"\n {8}\.frontAnimId = (.*),"
+                               + r"\n {8}\.backPic = (.*),"
+                               + r"(\n {8}\.backPicFemale = .*,|)"
+                               + r"\n {8}\.backPicSize = (.*),"
+                               + r"(\n {8}\.backPicSizeFemale = MON_COORDS_SIZE\(\w+, \w+\),|)"
+                               + r"(\n {8}\.backPicYOffset = .*,|)"
+                               + r"(\n {8}\.backAnimId = .*,|)"
+                               + r"\n {8}\.palette = (.*),"
+                               + r"(\n {8}\.paletteFemale = .*,|)"
+                               + r"\n {8}\.shinyPalette = (.*),"
+                               + r"(\n {8}\.shinyPaletteFemale = .*,|)"
+                               + r"\n {8}\.iconSprite = (.*),"
+                               + r"(\n {8}\.iconSpriteFemale = .*,|)"
+                               + r"\n {8}\.iconPalIndex = (.*),"
+                               + r"(\n {8}\.iconPalIndexFemale = .*,|)"
+                               #+ r"\n {8}FOOTPRINT\(Frillish\)"
+                            , r"        .frontPic = \1,"
+                            + r"\n        .frontPicSize = \3,"
+                            + r"\5"
+                            + r"\n        .frontAnimFrames = \6,"
+                            + r"\n        .frontAnimId = \7,"
+                            + r"\n        .backPic = \8,"
+                            + r"\n        .backPicSize = \10,"
+                            + r"\12"
+                            + r"\13"
+                            + r"\n        .palette = \14,"
+                            + r"\n        .shinyPalette = \16,"
+                            + r"\n        .iconSprite = \18,"
+                            + r"\n        .iconPalIndex = \20,"
+                            + r"\n#if P_GENDER_DIFFERENCES"
+                            + r"\2"
+                            + r"\4"
+                            + r"\9"
+                            + r"\11"
+                            + r"\15"
+                            + r"\17"
+                            + r"\19"
+                            + r"\21"
+                            + r"\n#endif //P_GENDER_DIFFERENCES"
+                            #+ r"\n        FOOTPRINT(Frillish)"
+                            , species_content
+        )
+
+        species_content = re.sub(r"#if P_GENDER_DIFFERENCES\n#endif //P_GENDER_DIFFERENCES\n", r"", species_content)
 
         # Alter front animations
         pattern = re.compile(r'\.frontAnimFrames = sAnims_(\w+),', re.DOTALL)
