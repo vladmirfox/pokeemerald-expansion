0c6d79b293bb5654d7316c3f667e9d80502a8b38
purrfectdoodle <purrfectdoodle@gmail.com>
2024-11-14 11:00:24 +0100

    porytiles compile.sh script - unborking all sets (except Uume to avoid conflicts w/ kithri)

---
+++ tools/porytiles/compile.sh
@@ -0,0 +1,38 @@
+# Exterior generic
+porytiles compile-primary -Wall -o ../../data/tilesets/primary/porytiles_primary_tutorial ./exterior-generic/ ../../include/constants/metatile_behaviors.h
+# Ginko
+porytiles compile-secondary -Wall -o ../../data/tilesets/secondary/ginko_woods ./ginko-woods/ ./exterior-generic ../../include/constants/metatile_behaviors.h
+# Sunrise
+porytiles compile-secondary -Wall -o ../../data/tilesets/secondary/sunrise_village ./sunrise-village ./exterior-generic ../../include/constants/metatile_behaviors.h
+# Saberside
+porytiles compile-secondary -Wall -o ../../data/tilesets/secondary/saberside/ ./saberside/ ./exterior-generic ../../include/constants/metatile_behaviors.h
+# Coreef
+porytiles compile-secondary -Wall -o ../../data/tilesets/secondary/coral/ ./coral/ ./exterior-generic ../../include/constants/metatile_behaviors.h
+# Open sea
+porytiles compile-secondary -Wall -o ../../data/tilesets/secondary/open_sea/ ./open-sea/ ./exterior-generic ../../include/constants/metatile_behaviors.h
+# Uume
+# porytiles compile-secondary -Wall -o ../../data/tilesets/secondary/uume_forest/ ./uume-forest/ ./exterior-generic ../../include/constants/metatile_behaviors.h
+# Saku
+porytiles compile-secondary -Wall -o ../../data/tilesets/secondary/saku_kura ./saku-kura ./exterior-generic ../../include/constants/metatile_behaviors.h
+# Silver tunnel
+porytiles compile-secondary -Wall -o ../../data/tilesets/secondary/silver_tunnel/ ./silver-tunnel/ ./exterior-generic ../../include/constants/metatile_behaviors.h
+# Soulkeep
+porytiles compile-secondary -Wall -o ../../data/tilesets/secondary/soulkeep/ ./soulkeep/ ./exterior-generic ../../include/constants/metatile_behaviors.h
+# Yifu
+porytiles compile-secondary -Wall -o ../../data/tilesets/secondary/yifu/ ./yifu/ ./exterior-generic ../../include/constants/metatile_behaviors.h
+# Decay
+porytiles compile-secondary -Wall -o ../../data/tilesets/secondary/decay/ ./decay/ ./exterior-generic ../../include/constants/metatile_behaviors.h
+
+# Buildings
+porytiles compile-primary -Wall -o ../../data/tilesets/primary/building-generic/ ./building-generic/ ../../include/constants/metatile_behaviors.h
+porytiles compile-secondary -Wall -o ../../data/tilesets/secondary/house_beige/ ./house-beige/ ./building-generic/ ../../include/constants/metatile_behaviors.h
+porytiles compile-secondary -Wall -o ../../data/tilesets/secondary/house_stone/ ./house-stone/ ./building-generic/ ../../include/constants/metatile_behaviors.h
+porytiles compile-secondary -Wall -o ../../data/tilesets/secondary/house_green/ ./house-green/ ./building-generic/ ../../include/constants/metatile_behaviors.h
+porytiles compile-secondary -Wall -o ../../data/tilesets/secondary/house_hardwood2/ ./house-hardwood2/ ./building-generic/ ../../include/constants/metatile_behaviors.h
+# Dojos
+porytiles compile-secondary -Wall -o ../../data/tilesets/secondary/dojo_maguro/ ./dojo-maguro/ ./building-generic/ ../../include/constants/metatile_behaviors.h
+porytiles compile-secondary -Wall -o ../../data/tilesets/secondary/dojo_kura/ ./dojo-kura/ ./building-generic/ ../../include/constants/metatile_behaviors.h
+# Refuge
+porytiles compile-secondary -Wall -o ../../data/tilesets/secondary/refuge/ ./refuge-shop/ ./building-generic/ ../../include/constants/metatile_behaviors.h
+# School
+porytiles compile-secondary -Wall -o ../../data/tilesets/secondary/school/ ./school/ ./building-generic/ ../../include/constants/metatile_behaviors.h
