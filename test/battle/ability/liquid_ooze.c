1e05536395bf74d9404839a955fa97417838e9b7
ghoulslash <pokevoyager0@gmail.com>
2024-12-01 14:17:52 -0500

    add liquid ooze dual faint test

---
+++ test/battle/ability/liquid_ooze.c
@@ -0,0 +1,28 @@
+#include "global.h"
+#include "test/battle.h"
+
+SINGLE_BATTLE_TEST("Liquid Ooze causes leech seedee to faint before seeder")
+{
+    u16 ability;
+    PARAMETRIZE { ability = ABILITY_CLEAR_BODY; }
+    PARAMETRIZE { ability = ABILITY_LIQUID_OOZE; }    
+    GIVEN {
+        PLAYER(SPECIES_BULBASAUR)   { HP(1); }
+        OPPONENT(SPECIES_TENTACOOL) { HP(1); Ability(ability); }
+    } WHEN {
+        TURN { MOVE(player, MOVE_LEECH_SEED); }
+    } SCENE {
+        MESSAGE("Bulbasaur used Leech Seed!");
+        // Drain at end of turn
+        ANIMATION(ANIM_TYPE_GENERAL, B_ANIM_LEECH_SEED_DRAIN, opponent);
+        if (ability != ABILITY_LIQUID_OOZE) {
+            MESSAGE("Foe Tentacool's health is sapped by Leech Seed!");
+            MESSAGE("Foe Tentacool fainted!");
+        } else {
+            ABILITY_POPUP(opponent, ABILITY_LIQUID_OOZE);
+            MESSAGE("It sucked up the liquid ooze!");
+            MESSAGE("Foe Tentacool fainted!");
+            MESSAGE("Bulbasaur fainted!");
+        }
+    }
+}
