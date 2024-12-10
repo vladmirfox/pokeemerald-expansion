a5a1055a11f2beb7a6330f6641c5dcd41188a8d8
Eduardo Quezada <eduardo602002@gmail.com>
2024-12-10 09:05:23 -0300

    8 tests

--- test/battle/move_effect/cosmic_power.c
+++ test/battle/move_effect/cosmic_power.c
@@ -1,4 +1,16 @@
 #include "global.h"
 #include "test/battle.h"
 
-TO_DO_BATTLE_TEST("Cosmic Power increases the user's Defense and Sp. Defense by 1 stage each");
+SINGLE_BATTLE_TEST("Cosmic Power increases the user's Defense and Sp. Defense by 1 stage each")
+{
+    GIVEN {
+        PLAYER(SPECIES_WOBBUFFET);
+        OPPONENT(SPECIES_WOBBUFFET);
+    } WHEN {
+        TURN { MOVE(player, MOVE_COSMIC_POWER); }
+    } SCENE {
+        ANIMATION(ANIM_TYPE_MOVE, MOVE_COSMIC_POWER, player);
+        MESSAGE("Wobbuffet's Defense rose!");
+        MESSAGE("Wobbuffet's Sp. Def rose!");
+    }
+}
