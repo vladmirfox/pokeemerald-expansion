e383cb259b2e3e9a7ace818f6ec6192d5f6b3bab
kittenchilly <mario.pikmin.zelda@gmail.com>
2024-12-20 11:08:35 -0600

    Update battle_scripts_1.s

--- data/battle_scripts_1.s
+++ data/battle_scripts_1.s
@@ -2791,8 +2791,10 @@ BattleScript_EffectHitEscape::
 	jumpifbattleend BattleScript_HitEscapeEnd
 	jumpifbyte CMP_NOT_EQUAL, gBattleOutcome, 0, BattleScript_HitEscapeEnd
 	jumpifemergencyexited BS_TARGET, BattleScript_HitEscapeEnd
+	jumpiffainted BS_TARGET, FALSE, BattleScript_HitEscapeSwitch
 	setbyte sGIVEEXP_STATE, 0
 	getexp BS_TARGET
+BattleScript_HitEscapeSwitch:
 	goto BattleScript_MoveSwitch
 BattleScript_HitEscapeEnd:
 	end
