c4cb5b507b831abf5fb23ee03d801ccabaab0159
Eduardo Quezada <eduardo602002@gmail.com>
2024-12-27 20:13:29 -0300

    Moved existing Dynamax Cannon test

--- test/battle/gimmick/dynamax.c
+++ test/battle/gimmick/dynamax.c
@@ -346,24 +346,6 @@ SINGLE_BATTLE_TEST("(DYNAMAX) Dynamaxed Pokemon lose their substitutes")
     }
 }
 
-SINGLE_BATTLE_TEST("(DYNAMAX) Dynamaxed Pokemon take double damage from Dynamax Cannon", s16 damage)
-{
-    u32 dynamax;
-    PARAMETRIZE { dynamax = GIMMICK_NONE; }
-    PARAMETRIZE { dynamax = GIMMICK_DYNAMAX; }
-    GIVEN {
-        ASSUME(gMovesInfo[MOVE_DYNAMAX_CANNON].effect == EFFECT_DYNAMAX_DOUBLE_DMG);
-        PLAYER(SPECIES_WOBBUFFET);
-        OPPONENT(SPECIES_WOBBUFFET);
-    } WHEN {
-        TURN { MOVE(player, MOVE_TACKLE, gimmick: dynamax); MOVE(opponent, MOVE_DYNAMAX_CANNON); }
-    } SCENE {
-        HP_BAR(player, captureDamage: &results[i].damage);
-    } FINALLY {
-        EXPECT_MUL_EQ(results[0].damage, UQ_4_12(2.0), results[1].damage);
-    }
-}
-
 SINGLE_BATTLE_TEST("(DYNAMAX) Max Moves deal 1/4 damage through protect", s16 damage)
 {
     bool32 protected;
