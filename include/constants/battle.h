05d8aa5b4e46aeaab3e2bab194d381a5a5d44198
AlexOn1ine <alexthenotes@gmail.com>
2024-12-29 12:18:11 +0100

    remove perma terrain flag

--- include/constants/battle.h
+++ include/constants/battle.h
@@ -266,7 +266,6 @@
 #define STATUS_FIELD_PSYCHIC_TERRAIN                (1 << 9)
 #define STATUS_FIELD_ION_DELUGE                     (1 << 10)
 #define STATUS_FIELD_FAIRY_LOCK                     (1 << 11)
-#define STATUS_FIELD_TERRAIN_PERMANENT              (1 << 12)   // Overworld thunderstorm generates electric terrain
 
 #define STATUS_FIELD_TERRAIN_ANY        (STATUS_FIELD_GRASSY_TERRAIN | STATUS_FIELD_MISTY_TERRAIN | STATUS_FIELD_ELECTRIC_TERRAIN | STATUS_FIELD_PSYCHIC_TERRAIN)
 
