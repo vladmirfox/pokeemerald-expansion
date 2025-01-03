03eb628e670408e366dd906a8c2ac0ee619a38e1
AlexOn1ine <alexthenotes@gmail.com>
2025-01-03 16:38:23 +0100

    fix test case

--- test/battle/ability/flash_fire.c
+++ test/battle/ability/flash_fire.c
@@ -3,8 +3,7 @@
 
 SINGLE_BATTLE_TEST("Flash Fire boosts fire type moves by 50% but no subsequent increase is applied")
 {
-    s16 normalDamage;
-    s16 boostedDamage;
+    s16 damage[3];
 
     GIVEN {
         PLAYER(SPECIES_HEATRAN) { Ability(ABILITY_FLASH_FIRE); }
@@ -15,12 +14,15 @@ SINGLE_BATTLE_TEST("Flash Fire boosts fire type moves by 50% but no subsequent i
         TURN { MOVE(player, MOVE_EMBER); }
     } SCENE {
         ANIMATION(ANIM_TYPE_MOVE, MOVE_EMBER, player);
-        HP_BAR(opponent, captureDamage: &normalDamage);
+        HP_BAR(opponent, captureDamage: &damage[0]);
         ABILITY_POPUP(player, ABILITY_FLASH_FIRE);
         ANIMATION(ANIM_TYPE_MOVE, MOVE_EMBER, player);
-        HP_BAR(opponent, captureDamage: &boostedDamage);
+        HP_BAR(opponent, captureDamage: &damage[1]);
         ABILITY_POPUP(player, ABILITY_FLASH_FIRE);
+        ANIMATION(ANIM_TYPE_MOVE, MOVE_EMBER, player);
+        HP_BAR(opponent, captureDamage: &damage[2]);
     } THEN {
-        EXPECT_MUL_EQ(normalDamage, UQ_4_12(1.5), boostedDamage);
+        EXPECT_MUL_EQ(damage[0], UQ_4_12(1.5), damage[1]);
+        EXPECT_EQ(damage[1], damage[2]);
     }
 }
