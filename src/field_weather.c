2798fa5268e733ea527201bd295556ef6496d6a2
Bivurnum <147376167+Bivurnum@users.noreply.github.com>
2025-01-08 13:48:08 -0600

    weather palette blends no longer affect map preview colors

--- src/field_weather.c
+++ src/field_weather.c
@@ -130,7 +130,7 @@ static const u8 ALIGNED(2) sBasePaletteColorMapTypes[32] =
     COLOR_MAP_DARK_CONTRAST,
     COLOR_MAP_DARK_CONTRAST,
     COLOR_MAP_DARK_CONTRAST,
-    COLOR_MAP_DARK_CONTRAST,
+    COLOR_MAP_NONE, // This was changed from COLOR_MAP_DARK_CONTRAST to make sure certain weather effects don't affect map preview colors.
     COLOR_MAP_NONE,
     COLOR_MAP_NONE,
     // sprite palettes
