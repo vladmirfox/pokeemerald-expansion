2b765f6745e88ca4d03a83afb636ec1f8099763c
pkmnsnfrn <pkmnsnfrn@gmail.com>
2025-01-10 22:18:31 -0800

    Add first critical hit test

--- test/battle/trainer_slides.c
+++ test/battle/trainer_slides.c
@@ -2,11 +2,6 @@
 #include "test/battle.h"
 #include "battle_setup.h"
 
-static const u8* const sTestTrainerSlides[DIFFICULTY_COUNT][TRAINERS_COUNT][TRAINER_SLIDE_COUNT] =
-{
-#include "../test/battle/trainer_slides.h"
-};
-
 SINGLE_BATTLE_TEST("Trainer Slide: Before First Turn")
 {
     gBattleTestRunnerState->data.recordedBattle.opponentA = TRAINER_SLIDE_BEFORE_FIRST_TURN;
@@ -18,5 +13,20 @@ SINGLE_BATTLE_TEST("Trainer Slide: Before First Turn")
         TURN { }
     } SCENE {
         MESSAGE("This message plays before the first turn.{PAUSE_UNTIL_PRESS}");
+    }
+}
+
+SINGLE_BATTLE_TEST("Trainer Slide: Player Lands First Critical Hit")
+{
+    gBattleTestRunnerState->data.recordedBattle.opponentA = TRAINER_SLIDE_PLAYER_LANDS_FIRST_CRITICAL_HIT;
+
+    GIVEN {
+        PLAYER(SPECIES_WOBBUFFET);
+        OPPONENT(SPECIES_WOBBUFFET);
+    } WHEN {
+        TURN { MOVE(player, MOVE_FROST_BREATH); }
+    } SCENE {
+        MESSAGE("A critical hit!");
+        MESSAGE("This message plays after the player lands their first critical hit.{PAUSE_UNTIL_PRESS}");
     }
 }
