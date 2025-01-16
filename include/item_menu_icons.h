67d5c03b0a29cd27512062904defe4ae643c8565
DizzyEggg <jajkodizzy@wp.pl>
2025-01-16 12:22:44 +0100

    more gDecompressionBuffer

--- include/item_menu_icons.h
+++ include/item_menu_icons.h
@@ -18,17 +18,17 @@ void CreateItemMenuSwapLine(void);
 void SetItemMenuSwapLineInvisibility(bool8 invisible);
 void UpdateItemMenuSwapLinePos(u8 y);
 
-// Note: Because of removing gDecompressionBuffer and allowing to create more than berry sprite at a time, the memory for gfx is allocated dynamically.
-// For CreateBerryTagSprite and CreateSpinningBerrySprite the allocated ptr is stored in two last data fields(data[6], data[7]), so make sure to NOT put anything in there!
+// Note: Because of removing gDecompressionBuffer and allowing to create more than one berry sprite at a time, the memory for gfx is allocated dynamically.
+// For CreateBerryTagSprite and CreateSpinningBerrySprite, the allocated ptr is stored in two last data fields(data[6], data[7]), so make sure to NOT put anything in there!
 // The corresponding code has already been edited in berry_tag_screen.c and berry_blender.c
 #define BERRY_ICON_GFX_PTR_DATA_ID 6
 u32 CreateBerryTagSprite(u32 id, s32 x, s32 y);
 u32 CreateSpinningBerrySprite(u32 berryId, s32 x, s32 y, bool32 startAffine);
-// Needs to call either of these always after creating a Berry Icon sprite, because it frees allocated memory!
+// Needs to always call either of these after creating a Berry Icon sprite, because it frees allocated memory!
 void DestroyBerryIconSprite(u32 spriteId, u32 berryId, bool32 freePal);
 void DestroyBerryIconSpritePtr(struct Sprite *sprite, u32 berryId, bool32 freePal);
 
-void FreeBerryIconSpritePalette(u32 berryId); // Unused atm, because it's also handled by DestroyBerryIconSprite. Leaving it as it still may be useful in some custom cases.
+void FreeBerryIconSpritePalette(u32 berryId); // Unused atm, because it's also handled by DestroyBerryIconSprite. Leaving it as it is, because it may still be useful in some custom cases.
 
 u8 CreateBerryFlavorCircleSprite(s16 x);
 
