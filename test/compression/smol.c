478ab023d5bdb088d9c441723ee7b0b8ba971aa2
Hedara <hedara90@gmail.com>
2025-01-19 22:36:29 +0100

    Added tests for Mr. Mime and Ledian

--- test/compression/smol.c
+++ test/compression/smol.c
@@ -245,6 +245,66 @@ TEST("Compression test: Gossifleur")
     Free(compBuffer);
 }
 
+TEST("Compression test: Ledian")
+{
+    static const u32 origFile[] = INCBIN_U32("test/compression/ledian.4bpp");
+    static const u32 compFile[] = INCBIN_U32("test/compression/ledian.4bpp.smol");
+    u32 imageSize = GetDecompressedDataSize(compFile);
+    u32 *compBuffer = Alloc(imageSize);
+    CycleCountStart();
+    DecompressDataWithHeaderWram(compFile, compBuffer);
+    u32 timeTaken = CycleCountEnd();
+    DebugPrintf("Ledian: %u", timeTaken);
+
+    u32 val1 = 0;
+    u32 val2 = 0;
+    bool32 areEqual = TRUE;
+    for (u32 i = 0; i < imageSize/4; i++)
+    {
+        val1 = origFile[i];
+        val2 = compBuffer[i];
+        if (val1 != val2)
+        {
+            areEqual = FALSE;
+            break;
+        }
+    }
+
+    EXPECT_EQ(areEqual, TRUE);
+
+    Free(compBuffer);
+}
+
+TEST("Compression test: Mr. Mime")
+{
+    static const u32 origFile[] = INCBIN_U32("test/compression/mr_mime.4bpp");
+    static const u32 compFile[] = INCBIN_U32("test/compression/mr_mime.4bpp.smol");
+    u32 imageSize = GetDecompressedDataSize(compFile);
+    u32 *compBuffer = Alloc(imageSize);
+    CycleCountStart();
+    DecompressDataWithHeaderWram(compFile, compBuffer);
+    u32 timeTaken = CycleCountEnd();
+    DebugPrintf("Ledian: %u", timeTaken);
+
+    u32 val1 = 0;
+    u32 val2 = 0;
+    bool32 areEqual = TRUE;
+    for (u32 i = 0; i < imageSize/4; i++)
+    {
+        val1 = origFile[i];
+        val2 = compBuffer[i];
+        if (val1 != val2)
+        {
+            areEqual = FALSE;
+            break;
+        }
+    }
+
+    EXPECT_EQ(areEqual, TRUE);
+
+    Free(compBuffer);
+}
+
 TEST("Compression test: bubbles")
 {
     static const u32 origFile[] = INCBIN_U32("test/compression/small_bubbles.4bpp");
