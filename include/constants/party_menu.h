9b5e9b2750caa07498d4100843f2de0fbcf1e61a
Zatsu <118256341+fakuzatsu@users.noreply.github.com>
2025-01-20 12:12:55 +0000

    Remove incorrectly added string definition

--- include/constants/party_menu.h
+++ include/constants/party_menu.h
@@ -82,8 +82,7 @@
 #define PARTY_MSG_WHICH_APPLIANCE           27
 #define PARTY_MSG_CHOOSE_SECOND_FUSION      28
 #define PARTY_MSG_NO_POKEMON                29
-#define PARTY_MSG_MON_NOT_FOLLOWING         30
-#define PARTY_MSG_CHOOSE_MON_FOR_BOX        31
+#define PARTY_MSG_CHOOSE_MON_FOR_BOX        30
 
 #define PARTY_MSG_NONE                      127
 
