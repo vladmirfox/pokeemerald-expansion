fa88129754502fda81ccbf4497d35609da371ecd
Hedara <hedara90@gmail.com>
2025-01-21 17:55:18 +0100

    Secondary tileset test

--- test/compression/smol.c
+++ test/compression/smol.c
@@ -95,6 +95,95 @@ TEST("Compression test: tileset LZ")
     Free(compBuffer);
 }
 
+TEST("Compression test: secondary tileset smol")
+{
+    static const u32 origFile[] = INCBIN_U32("test/compression/secondary_tileset.4bpp");
+    static const u32 compFile[] = INCBIN_U32("test/compression/secondary_tileset.4bpp.smol");
+    u32 imageSize = GetDecompressedDataSize(compFile);
+    u32 *compBuffer = Alloc(imageSize);
+    CycleCountStart();
+    DecompressDataWithHeaderWram(compFile, compBuffer);
+    u32 timeTaken = CycleCountEnd();
+    DebugPrintf("Time Secondary Tileset Smol: %u", timeTaken);
+
+    u32 val1 = 0;
+    u32 val2 = 0;
+    bool32 areEqual = TRUE;
+    for (u32 i = 0; i < imageSize/4; i++)
+    {
+        val1 = origFile[i];
+        val2 = compBuffer[i];
+        if (val1 != val2)
+        {
+            areEqual = FALSE;
+            break;
+        }
+    }
+
+    EXPECT_EQ(areEqual, TRUE);
+
+    Free(compBuffer);
+}
+
+TEST("Compression test: secondary tileset fastSmol")
+{
+    static const u32 origFile[] = INCBIN_U32("test/compression/secondary_tileset.4bpp");
+    static const u32 compFile[] = INCBIN_U32("test/compression/secondary_tileset.4bpp.fastSmol");
+    u32 imageSize = GetDecompressedDataSize(compFile);
+    u32 *compBuffer = Alloc(imageSize);
+    CycleCountStart();
+    DecompressDataWithHeaderWram(compFile, compBuffer);
+    u32 timeTaken = CycleCountEnd();
+    DebugPrintf("Time Secondary Tileset fastSmol: %u", timeTaken);
+
+    u32 val1 = 0;
+    u32 val2 = 0;
+    bool32 areEqual = TRUE;
+    for (u32 i = 0; i < imageSize/4; i++)
+    {
+        val1 = origFile[i];
+        val2 = compBuffer[i];
+        if (val1 != val2)
+        {
+            areEqual = FALSE;
+            break;
+        }
+    }
+
+    EXPECT_EQ(areEqual, TRUE);
+
+    Free(compBuffer);
+}
+
+TEST("Compression test: secondary tileset LZ")
+{
+    static const u32 origFile[] = INCBIN_U32("test/compression/secondary_tileset.4bpp");
+    static const u32 compFile[] = INCBIN_U32("test/compression/secondary_tileset.4bpp.lz");
+    u32 imageSize = GetDecompressedDataSize(compFile);
+    u32 *compBuffer = Alloc(imageSize);
+    CycleCountStart();
+    DecompressDataWithHeaderWram(compFile, compBuffer);
+    u32 timeTaken = CycleCountEnd();
+    DebugPrintf("Time Secondary Tileset LZ: %u", timeTaken);
+
+    u32 val1 = 0;
+    u32 val2 = 0;
+    bool32 areEqual = TRUE;
+    for (u32 i = 0; i < imageSize/4; i++)
+    {
+        val1 = origFile[i];
+        val2 = compBuffer[i];
+        if (val1 != val2)
+        {
+            areEqual = FALSE;
+            break;
+        }
+    }
+
+    EXPECT_EQ(areEqual, TRUE);
+
+    Free(compBuffer);
+}
 TEST("Compression test: simple battle sprite smol")
 {
     static const u32 origFile[] = INCBIN_U32("test/compression/simple_battle_sprite.4bpp");
