8570e39298b639153af74bbe40edba2245075555
Hedara <hedara90@gmail.com>
2025-01-26 22:29:13 +0100

    Fixed

--- test/compression/smol.c
+++ test/compression/smol.c
@@ -6148,8 +6148,8 @@ TEST("Compression test: tilemap fastLZ")
 
 TEST("Compression test: tilemap large smolTM")
 {
-    static const u16 origFile[] = INCBIN_U16("test/compression/tilemap.large.bin");
-    static const u32 compFile[] = INCBIN_U32("test/compression/tilemap.large.bin.smolTM");
+    static const u16 origFile[] = INCBIN_U16("test/compression/tilemapLarge.bin");
+    static const u32 compFile[] = INCBIN_U32("test/compression/tilemapLarge.bin.smolTM");
 
     bool32 areEqual = DecompressTilemapPrintResults(compFile, origFile, "Tilemap large", COMPRESSION_SMOL_TILEMAP, sizeof(compFile));
     EXPECT_EQ(areEqual, TRUE);
@@ -6157,8 +6157,8 @@ TEST("Compression test: tilemap large smolTM")
 
 TEST("Compression test: tilemap large LZ")
 {
-    static const u16 origFile[] = INCBIN_U16("test/compression/tilemap.large.bin");
-    static const u32 compFile[] = INCBIN_U32("test/compression/tilemap.large.bin.lz");
+    static const u16 origFile[] = INCBIN_U16("test/compression/tilemapLarge.bin");
+    static const u32 compFile[] = INCBIN_U32("test/compression/tilemapLarge.bin.lz");
 
     bool32 areEqual = DecompressTilemapPrintResults(compFile, origFile, "Tilemap large ", COMPRESSION_LZ, sizeof(compFile));
     EXPECT_EQ(areEqual, TRUE);
@@ -6166,8 +6166,8 @@ TEST("Compression test: tilemap large LZ")
 
 TEST("Compression test: tilemap large fastLZ")
 {
-    static const u16 origFile[] = INCBIN_U16("test/compression/tilemap.large.bin");
-    static const u32 compFile[] = INCBIN_U32("test/compression/tilemap.large.bin.lz");
+    static const u16 origFile[] = INCBIN_U16("test/compression/tilemapLarge.bin");
+    static const u32 compFile[] = INCBIN_U32("test/compression/tilemapLarge.bin.lz");
 
     bool32 areEqual = DecompressTilemapPrintResults(compFile, origFile, "Tilemap large", COMPRESSION_FASTLZ, sizeof(compFile));
     EXPECT_EQ(areEqual, TRUE);
