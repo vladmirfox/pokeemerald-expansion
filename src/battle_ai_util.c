5d66689da3e7c971fe7f04bcb9e1c6e714957967
Moos Toet <moostoet@hotmail.com>
2025-02-15 14:25:18 +0100

    fix: priority moves considered even if player used priority before

--- src/battle_ai_util.c
+++ src/battle_ai_util.c
@@ -1068,7 +1068,14 @@ s32 AI_WhoStrikesFirst(u32 battlerAI, u32 battler, u32 moveConsidered)
     u32 abilityAI = AI_DATA->abilities[battlerAI];
     u32 abilityPlayer = AI_DATA->abilities[battler];
 
-    if (GetBattleMovePriority(battlerAI, moveConsidered) > 0)
+    u32 predictedMove = AI_DATA->lastUsedMove[battler];
+
+    s8 aiPriority = GetBattleMovePriority(battlerAI, moveConsidered);
+    s8 playerPriority = GetBattleMovePriority(battler, predictedMove);
+
+    if (aiPriority > playerPriority)
+        return AI_IS_SLOWER;
+    else if (aiPriority < playerPriority)
         return AI_IS_FASTER;
 
     speedBattlerAI = GetBattlerTotalSpeedStatArgs(battlerAI, abilityAI, holdEffectAI);
