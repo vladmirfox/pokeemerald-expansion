40c96110ebaa410a9e7802e8799c6db6683c1204
Bivurnum <147376167+Bivurnum@users.noreply.github.com>
2025-02-18 14:32:17 -0600

    renamed follower flags for clarity

--- include/constants/follow_me.h
+++ include/constants/follow_me.h
@@ -1,30 +1,30 @@
 #ifndef GUARD_CONSTANTS_FOLLOW_ME_H
 #define GUARD_CONSTANTS_FOLLOW_ME_H
 
-#define FOLLOWER_FLAG_HAS_RUNNING_FRAMES    0x1
-#define FOLLOWER_FLAG_CAN_BIKE              0x2
-#define FOLLOWER_FLAG_CAN_LEAVE_ROUTE       0x4     // teleport, dig, fly, etc
-#define FOLLOWER_FLAG_CAN_SURF              0x8
-#define FOLLOWER_FLAG_CAN_WATERFALL         0x10
-#define FOLLOWER_FLAG_CAN_DIVE              0x20
-#define FOLLOWER_FLAG_CLEAR_ON_WHITE_OUT    0x80
+#define FOLLOW_ME_FLAG_HAS_RUNNING_FRAMES    0x1
+#define FOLLOW_ME_FLAG_CAN_BIKE              0x2
+#define FOLLOW_ME_FLAG_CAN_LEAVE_ROUTE       0x4     // teleport, dig, fly, etc
+#define FOLLOW_ME_FLAG_CAN_SURF              0x8
+#define FOLLOW_ME_FLAG_CAN_WATERFALL         0x10
+#define FOLLOW_ME_FLAG_CAN_DIVE              0x20
+#define FOLLOW_ME_FLAG_CLEAR_ON_WHITE_OUT    0x80
 
-#define FOLLOWER_FLAG_ALL_LAND              FOLLOWER_FLAG_HAS_RUNNING_FRAMES | FOLLOWER_FLAG_CAN_BIKE | FOLLOWER_FLAG_CAN_LEAVE_ROUTE
-#define FOLLOWER_FLAG_ALL_WATER             FOLLOWER_FLAG_CAN_SURF | FOLLOWER_FLAG_CAN_WATERFALL | FOLLOWER_FLAG_CAN_DIVE
-#define FOLLOWER_FLAG_ALL                   FOLLOWER_FLAG_ALL_LAND | FOLLOWER_FLAG_ALL_WATER | FOLLOWER_FLAG_CLEAR_ON_WHITE_OUT
+#define FOLLOW_ME_FLAG_ALL_LAND              FOLLOW_ME_FLAG_HAS_RUNNING_FRAMES | FOLLOW_ME_FLAG_CAN_BIKE | FOLLOW_ME_FLAG_CAN_LEAVE_ROUTE
+#define FOLLOW_ME_FLAG_ALL_WATER             FOLLOW_ME_FLAG_CAN_SURF | FOLLOW_ME_FLAG_CAN_WATERFALL | FOLLOW_ME_FLAG_CAN_DIVE
+#define FOLLOW_ME_FLAG_ALL                   FOLLOW_ME_FLAG_ALL_LAND | FOLLOW_ME_FLAG_ALL_WATER | FOLLOW_ME_FLAG_CLEAR_ON_WHITE_OUT
 
 //For ease of use in setfollower macro while scripting
-#define FM_RUNNING                          FOLLOWER_FLAG_HAS_RUNNING_FRAMES
-#define FM_BIKE                             FOLLOWER_FLAG_CAN_BIKE
-#define FM_LEAVE_ROUTE                      FOLLOWER_FLAG_CAN_LEAVE_ROUTE
-#define FM_SURF                             FOLLOWER_FLAG_CAN_SURF
-#define FM_WATERFALL                        FOLLOWER_FLAG_CAN_WATERFALL
-#define FM_DIVE                             FOLLOWER_FLAG_CAN_DIVE
-#define FM_WHITE_OUT                        FOLLOWER_FLAG_CLEAR_ON_WHITE_OUT
+#define FM_RUNNING                          FOLLOW_ME_FLAG_HAS_RUNNING_FRAMES
+#define FM_BIKE                             FOLLOW_ME_FLAG_CAN_BIKE
+#define FM_LEAVE_ROUTE                      FOLLOW_ME_FLAG_CAN_LEAVE_ROUTE
+#define FM_SURF                             FOLLOW_ME_FLAG_CAN_SURF
+#define FM_WATERFALL                        FOLLOW_ME_FLAG_CAN_WATERFALL
+#define FM_DIVE                             FOLLOW_ME_FLAG_CAN_DIVE
+#define FM_WHITE_OUT                        FOLLOW_ME_FLAG_CLEAR_ON_WHITE_OUT
 
-#define FM_ALL_LAND                         FOLLOWER_FLAG_ALL_LAND
-#define FM_ALL_WATER                        FOLLOWER_FLAG_ALL_WATER
-#define FM_ALL                              FOLLOWER_FLAG_ALL
+#define FM_ALL_LAND                         FOLLOW_ME_FLAG_ALL_LAND
+#define FM_ALL_WATER                        FOLLOW_ME_FLAG_ALL_WATER
+#define FM_ALL                              FOLLOW_ME_FLAG_ALL
 
 
 // Replace the 0 with a flag in order to use that flag to toggle whether the Player's party
@@ -44,12 +44,6 @@
 
 // If set to TRUE, the player will turn to face the follower when they exit a doorway.
 #define FACE_FOLLOWER_ON_DOOR_EXIT          TRUE
-
-
-
-// These flags are currently bugged and may cause the game to softlock.
-// They have been removed from all of the defines above.
-// Use at your own risk.
 
 
 #endif // GUARD_CONSTANTS_FOLLOW_ME_H
