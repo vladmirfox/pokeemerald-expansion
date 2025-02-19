1fa7c8e80193b4ec8d6472e8d1c769a1f0b5d7dc
Hedara <hedara90@gmail.com>
2025-02-19 13:25:55 +0100

    Move animation test

--- include/config/test.h
+++ include/config/test.h
@@ -1137,4 +1137,7 @@
 #undef B_FLAG_SLEEP_CLAUSE
 #define B_FLAG_SLEEP_CLAUSE              FLAG_SPECIAL_FLAG_UNUSED_0x4003
 
+//  Move animation testing
+#define T_SHOULD_RUN_MOVE_ANIM  FALSE       //  If TRUE, enables the move animation tests, these are very computationally heavy and takes a long time to run.
+
 #endif // GUARD_CONFIG_TEST_H
