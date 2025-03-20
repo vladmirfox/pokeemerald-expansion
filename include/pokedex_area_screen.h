8707330f9b45330e74a5a3d7f119ec778828807c
khbsd <khb.dev@gmail.com>
2025-03-19 20:56:31 -0500

    fix: remove unneeded enum

--- include/pokedex_area_screen.h
+++ include/pokedex_area_screen.h
@@ -3,12 +3,6 @@
 
 extern u32 gAreaTimeOfDay;
 
-enum ChangeTimeOfDay 
-{
-    DEX_AREA_INC_TIME_OF_DAY,
-    DEX_AREA_DEC_TIME_OF_DAY
-};
-
 void ShowPokedexAreaScreen(u16 species, u8 *screenSwitchState, u32 timeOfDay);
 
 #endif // GUARD_POKEDEX_AREA_SCREEN_H
